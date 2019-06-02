3b6d870c87134e4895ad3cf7cebe97aaa2b414bd
Aaron Hsu <aaron@nextbit.com>
2019-06-03 06:08:43 +0900

    rename App to Home

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styl/index.css';
-import App from './js/App';
+import Home from './js/home.js';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/js/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styl/home.css';
+
+function Home() {
+  return (
+    <p>Math Navigator</p>
+  );
+}
+
+export default Home;
