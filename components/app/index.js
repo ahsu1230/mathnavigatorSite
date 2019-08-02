ddbb206d37b189e2bcab06515f46272b81892460
Aaron Hsu <aaron@nextbit.com>
2019-08-02 14:11:12 -0400

    Add babel-polyfill

--- components/app/index.js
+++ components/app/index.js
@@ -1,6 +1,7 @@
 'use strict';
 import 'core-js/es/map';
 import 'core-js/es/set';
+import 'babel-polyfill';
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
