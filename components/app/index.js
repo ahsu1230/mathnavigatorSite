5aa94c61d6789774d8021d8e083014a84dbb9fc3
Aaron Hsu <aaron@nextbit.com>
2019-08-02 21:18:31 -0400

    Remove unnecessary dependencies

--- components/app/index.js
+++ components/app/index.js
@@ -1,11 +1,4 @@
 'use strict';
-import 'core-js/es/map';
-import 'core-js/es/set';
-import 'babel-polyfill';
-import 'react-app-polyfill/ie9';
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
-
 require('./app.styl');
 import React from 'react';
 import ReactDOM from 'react-dom';
