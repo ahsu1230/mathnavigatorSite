cc56fa72972c26084cc0a75b3678ecd9954f20ac
amymath <andymathnavigator@gmail.com>
2019-08-07 12:15:01 -0400

    adding testJson.batch file

---
+++ testJson.bat
@@ -0,0 +1,21 @@
+set target=C:\Users\Amylin\Downloads\jsons\*
+
+dir %target%
+
+@echo off
+
+
+
+@echo off
+set count=0
+for %%x in (%target%.json) do set /a count+=1
+echo %count%
+
+IF %count% GTR 0 (
+	echo files available!
+	move %target%.json components\repos\json
+)ELSE (
+	echo no files!
+)
+
+npm run-script run
