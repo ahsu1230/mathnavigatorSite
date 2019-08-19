a134c400a380451afc64b8ab267a6b3305e27e68
Aaron Hsu <aaron@nextbit.com>
2019-08-19 12:23:07 -0400

    Persist home announcement dismiss into localStorage

--- components/home/homeAnnounce.js
+++ components/home/homeAnnounce.js
@@ -3,8 +3,9 @@ require('./home.styl');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-import { getAnnounceList } from '../repos/mainRepo.js';
 import { isEmpty, filter } from 'lodash';
+import { getAnnounceList } from '../repos/mainRepo.js';
+import { ANNOUNCE_LAST_DISMISS } from '../storage.js';
 const classnames = require('classnames');
 const srcClose = require('../../assets/close_black.svg');
 
@@ -14,12 +15,16 @@ export class HomeAnnounce extends React.Component {
     this.state = {
       show: false
     }
-    var valid = filter(getAnnounceList(), a => a.onHomePage);
-    this.targetAnnounce = valid.length > 0 ? valid[0] : undefined;
     this.handleDismiss = this.handleDismiss.bind(this);
   }
 
   componentDidMount() {
+    var valid = filter(getAnnounceList(), a => a.onHomePage);
+    this.targetAnnounce = valid.length > 0 ? valid[0] : undefined;
+
+    var localLastDismiss = localStorage.getItem(ANNOUNCE_LAST_DISMISS)
+    this.lastDismissedAnnounce = parseInt(localLastDismiss, 10) || 0;
+
     this.unbindTimeout = setTimeout(function() {
       this.setState({ show: true });
     }.bind(this), 2500);
@@ -31,12 +36,15 @@ export class HomeAnnounce extends React.Component {
 
   handleDismiss() {
     this.setState({show: false});
+    localStorage.setItem(ANNOUNCE_LAST_DISMISS, this.targetAnnounce.timestamp);
   }
 
 	render() {
     const announce = this.targetAnnounce;
+    var showAnnounce = !isEmpty(announce) && (announce.timestamp > this.lastDismissedAnnounce);
+
     var component;
-    if (!isEmpty(announce)) {
+    if (showAnnounce) {
       component = generateAnnounce(announce, this.state.show, this.handleDismiss);
     } else {
       component = <div></div>;
---
+++ components/storage.js
@@ -0,0 +1,4 @@
+// Local Storage Constants
+
+// Stores timestamp of last announcement dismissed
+export const ANNOUNCE_LAST_DISMISS = "announce_last_dismiss";
