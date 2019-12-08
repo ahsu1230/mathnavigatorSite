eb451a9a8d32d0d54c0ddc87d17dd1afaad3db02
Aaron Hsu <aaron@nextbit.com>
2019-12-08 16:42:32 -0500

    setup webpack for sites home

---
+++ orion/sites/home/src/index.js
@@ -0,0 +1,42 @@
+'use strict';
+// require('./app.styl');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router';
+import {
+  HashRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import { HomePage } from './home/home.js';
+
+const Home = () => <HomePage/>;
+
+class AppContainer extends React.Component {
+	render() {
+		return (
+      <Router>
+        <AppWithRouter/>
+      </Router>
+		);
+	}
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const AppWithRouter = withRouter(App);
+
+ReactDOM.render(
+  <AppContainer/>,
+  document.getElementById('root')
+);
