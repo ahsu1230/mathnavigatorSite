b21c905697039cadd2ca5186b28e6dbc8f3d9826
Aaron Hsu <aaron@nextbit.com>
2019-12-15 19:35:57 -0500

    admin site points to load balancer domains

--- orion/sites/admin/src/api.js
+++ orion/sites/admin/src/api.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 var BASE_HOST;
 if (process.env.NODE_ENV == 'production') {
-  BASE_HOST = "http://34.222.112.53"; // replace later
+  BASE_HOST = "http://lb-prod-webserver-678749426.us-west-2.elb.amazonaws.com";
 } else if (process.env.NODE_ENV == 'development') {
-  BASE_HOST = "http://52.38.235.34"; // replace later
+  BASE_HOST = "http://lb-dev-webserver-2018209767.us-west-2.elb.amazonaws.com";
 } else {
   BASE_HOST = "http://localhost:8080";
 }
