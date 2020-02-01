c2100e91d5f6b6e6e99226cd2ff5c4085f3f206a
Aaron Hsu <aaron@nextbit.com>
2020-02-01 11:28:31 -0500

    First sql migration file

---
+++ orion/database/migrations/1_init_programs.up.sql
@@ -0,0 +1,14 @@
+USE SCHEMA `mathnavdb`;
+DROP TABLE IF EXISTS `programs`;
+CREATE TABLE `programs` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` bigint unsigned NOT NULL,
+  `updated_at` bigint unsigned NOT NULL,
+  `deleted_at` bigint unsigned,
+  `program_id` varchar(64) NOT NULL UNIQUE,
+  `name` varchar(255) NOT NULL,
+  `grade1` tinyint unsigned NOT NULL,
+  `grade2` tinyint unsigned NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1 DEFAULT CHARSET=UTF8MB4;
