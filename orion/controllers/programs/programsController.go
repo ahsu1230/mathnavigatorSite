aeb8f6c4efb614c1d1df2b3f0dc347f2ea21035d
Tony Wu <tonywu315@gmail.com>
2020-02-13 20:52:52 -0500

    Changed regex, test cases, error messages, and indents

--- orion/controllers/programs/programsController.go
+++ orion/controllers/programs/programsController.go
@@ -1,125 +1,121 @@
 package programs
 
 import (
-  "errors"
-  "net/http"
-  "regexp"
-  "github.com/gin-gonic/gin"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"regexp"
 )
 
-const REGEX_PROGRAM_ID = "^[[:alnum:]]+(_[[:alnum:]]+)*$"
+const REGEX_PROGRAM_ID = `^[[:alnum:]]+(_[[:alnum:]]+)*$`
+const REGEX_NAME = `^[A-Z0-9][\w-]*([- _]([(]?#\d[)]?|&|([(]?[[:alnum:]]+[)]?)))*$`
 
 func GetPrograms(c *gin.Context) {
-  // Query Repo
-  programList := GetAllPrograms()
+	// Query Repo
+	programList := GetAllPrograms()
 
-  // JSON Response
-  c.JSON(http.StatusOK, programList)
-  return
+	// JSON Response
+	c.JSON(http.StatusOK, programList)
+	return
 }
 
 func GetProgram(c *gin.Context) {
-  // Incoming parameters
-  programId := c.Param("programId")
+	// Incoming parameters
+	programId := c.Param("programId")
 
-  // Query Repo
-  program, err := GetProgramById(programId)
-  if err != nil {
-    panic(err)
-  } else {
-    c.JSON(http.StatusOK, program)
-  }
-  return
+	// Query Repo
+	program, err := GetProgramById(programId)
+	if err != nil {
+		panic(err)
+	} else {
+		c.JSON(http.StatusOK, program)
+	}
+	return
 }
 
 func CreateProgram(c *gin.Context) {
-  // Incoming JSON
-  var programJson Program
-  c.BindJSON(&programJson)
+	// Incoming JSON
+	var programJson Program
+	c.BindJSON(&programJson)
 
-  if err := CheckValidProgram(programJson); err != nil {
-    c.String(http.StatusBadRequest, err.Error())
-    return
-  }
+	if err := CheckValidProgram(programJson); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
 
-  // Query Repo (INSERT & SELECT)
-  err := InsertProgram(programJson)
-  if err != nil {
-    panic(err)
-  } else {
-    c.JSON(http.StatusOK, nil)
-  }
-  return
+	// Query Repo (INSERT & SELECT)
+	err := InsertProgram(programJson)
+	if err != nil {
+		panic(err)
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+	return
 }
 
 func UpdateProgram(c *gin.Context) {
-  // Incoming JSON & Parameters
-  programId := c.Param("programId")
-  var programJson Program
-  c.BindJSON(&programJson)
+	// Incoming JSON & Parameters
+	programId := c.Param("programId")
+	var programJson Program
+	c.BindJSON(&programJson)
 
-  if err := CheckValidProgram(programJson); err != nil {
-    c.String(http.StatusBadRequest, err.Error())
-    return
-  }
+	if err := CheckValidProgram(programJson); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
 
-  // Query Repo (UPDATE & SELECT)
-  err := UpdateProgramById(programId, programJson)
-  if err != nil {
-    panic(err)
-  } else {
-    c.JSON(http.StatusOK, nil)
-  }
-  return
+	// Query Repo (UPDATE & SELECT)
+	err := UpdateProgramById(programId, programJson)
+	if err != nil {
+		panic(err)
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+	return
 }
 
 func DeleteProgram(c *gin.Context) {
-  // Incoming Parameters
-  programId := c.Param("programId")
+	// Incoming Parameters
+	programId := c.Param("programId")
 
-  // Query Repo (DELETE)
-  err := DeleteProgramById(programId)
-  if err != nil {
-    panic(err)
-  } else {
-    c.String(http.StatusOK, "Deleted Program " + programId)
-  }
-  return
+	// Query Repo (DELETE)
+	err := DeleteProgramById(programId)
+	if err != nil {
+		panic(err)
+	} else {
+		c.String(http.StatusOK, "Deleted Program "+programId)
+	}
+	return
 }
 
 func CheckValidProgram(program Program) error {
-  // Retrieves the inputted values
-  programId := program.ProgramId
-  name := program.Name
-  grade1 := program.Grade1
-  grade2 := program.Grade2
-  description := program.Description
+	// Retrieves the inputted values
+	programId := program.ProgramId
+	name := program.Name
+	grade1 := program.Grade1
+	grade2 := program.Grade2
+	description := program.Description
 
-  // Ensures program ID isn't empty
-  if programId == "" {
-    return errors.New("Empty programId.")
-  }
+	// Checks if the program ID is in the form of alphanumeric strings separated by underscores
+	if matches, _ := regexp.MatchString(REGEX_PROGRAM_ID, programId); !matches {
+		return errors.New("invalid program id")
+	}
 
-  // Checks if the program ID is in the form of alphanumeric strings separated by underscores
-  if matches, _ := regexp.MatchString(REGEX_PROGRAM_ID, programId); !matches {
-    return errors.New("Invalid programId.")
-  }
-  
-  // Name validation
-  match, _ := regexp.MatchString(`^[A-Z0-9]\S*(\s[A-Z0-9]\S*)*$`, name)
-  if !match {
-  	return errors.New("Invalid name.")
-  }
+	// Name validation
+	match, _ := regexp.MatchString(REGEX_NAME, name)
+	if !match {
+		return errors.New("invalid program name")
+	}
 
-  // Checks if the grades are valid
-  if !(grade1 <= grade2 && grade1 >= 1 && grade2 <= 12) {
-    return errors.New("Invalid grades.")
-  }
-  
-  // Description validation
-  if description == "" {
-    return errors.New("Empty description.")
-  }
+	// Checks if the grades are valid
+	if !(grade1 <= grade2 && grade1 >= 1 && grade2 <= 12) {
+		return errors.New("invalid grades")
+	}
 
-  return nil
+	// Description validation
+	if description == "" {
+		return errors.New("empty description")
+	}
+
+	return nil
 }
