0b8408b486e74147c1753cd69c202dfe60c9f4da
Tony Wu <tonywu315@gmail.com>
2020-02-20 18:22:23 -0500

    Connected achievement controller to API endpoints

--- orion/main.go
+++ orion/main.go
@@ -1,62 +1,71 @@
 package main
+
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/gin-contrib/cors"
-  "github.com/gin-gonic/gin"
-  "github.com/gin-gonic/contrib/static"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/contrib/static"
 
-  "github.com/ahsu1230/mathnavigatorSite/orion/controllers/programs"
-  "github.com/ahsu1230/mathnavigatorSite/orion/middlewares"
-  "github.com/ahsu1230/mathnavigatorSite/orion/database"
+	"github.com/ahsu1230/mathnavigatorSite/orion/controllers/programs"
+	"github.com/ahsu1230/mathnavigatorSite/orion/controllers/achieve"
+	"github.com/ahsu1230/mathnavigatorSite/orion/middlewares"
+	"github.com/ahsu1230/mathnavigatorSite/orion/database"
 )
 
 func main() {
-  fmt.Println("Orion service starting...")
+	fmt.Println("Orion service starting...")
 
-  config := middlewares.RetrieveConfigurations()
-  fmt.Println("Building server in mode: ", config.App.Build)
+	config := middlewares.RetrieveConfigurations()
+	fmt.Println("Building server in mode: ", config.App.Build)
 
-  fmt.Println("Connecting to DB...")
-  configDb := config.Database
-  database.OpenDb(configDb.Host, configDb.Port,
-    configDb.Username, configDb.Password)
-  fmt.Println("Performing DB Migrations...")
-  database.Migrate()
+	fmt.Println("Connecting to DB...")
+	configDb := config.Database
+	database.OpenDb(configDb.Host, configDb.Port,
+		configDb.Username, configDb.Password)
+	fmt.Println("Performing DB Migrations...")
+	database.Migrate()
 
-  fmt.Println("Setting up Router...")
-  router := gin.Default()
+	fmt.Println("Setting up Router...")
+	router := gin.Default()
 
-  fmt.Println("Setting up Middlewares...")
+	fmt.Println("Setting up Middlewares...")
 
-  // CORS middleware
-  configCors := middlewares.CreateCorsConfig(config);
-  router.Use(cors.New(configCors))
+	// CORS middleware
+	configCors := middlewares.CreateCorsConfig(config)
+	router.Use(cors.New(configCors))
 
-  // Webpage Routers
-  // router.Use(static.Serve("/", static.LocalFile("./sites/home", true)))
-  router.Use(static.Serve("/", static.LocalFile("./sites/admin", true)))
+	// Webpage Routers
+	// router.Use(static.Serve("/", static.LocalFile("./sites/home", true)))
+	router.Use(static.Serve("/", static.LocalFile("./sites/admin", true)))
 
-  // API Routers
-  apiPrograms := router.Group("/api/programs/")
-  {
-    apiPrograms.GET("/v1/all", programs.GetPrograms)
-    apiPrograms.POST("/v1/create", programs.CreateProgram)
-    apiPrograms.GET("/v1/program/:programId", programs.GetProgram)
-    apiPrograms.POST("/v1/program/:programId", programs.UpdateProgram)
-    apiPrograms.DELETE("/v1/program/:programId", programs.DeleteProgram)
-  }
-  // apiClasses := router.Group("api/classes/")
-  // apiLocations := router.Group("api/locations/")
-  // apiAnnounce := router.Group("api/announce/")
-  // apiAchieve := router.Group("api/achieve/")
-  // apiSemesters := router.Group("api/semesters/")
-  // apiUsers := router.Group("api/users/")
-  // apiAccounts := router.Group("api/accounts/")
+	// API Routers
+	apiPrograms := router.Group("/api/programs/")
+	{
+		apiPrograms.GET("/v1/all", programs.GetPrograms)
+		apiPrograms.POST("/v1/create", programs.CreateProgram)
+		apiPrograms.GET("/v1/program/:programId", programs.GetProgram)
+		apiPrograms.POST("/v1/program/:programId", programs.UpdateProgram)
+		apiPrograms.DELETE("/v1/program/:programId", programs.DeleteProgram)
+	}
+	// apiClasses := router.Group("api/classes/")
+	// apiLocations := router.Group("api/locations/")
+	// apiAnnounce := router.Group("api/announce/")
+	apiAchieve := router.Group("api/achieve/")
+	{
+		apiAchieve.GET("/v1/all", achieve.GetAchievements)
+		apiAchieve.POST("/v1/create", achieve.CreateAchievement)
+		apiAchieve.GET("/v1/achieve/:achieveId", achieve.GetAchievement)
+		apiAchieve.POST("/v1/achieve/:achieveId", achieve.UpdateAchievement)
+		apiAchieve.DELETE("/v1/achieve/:achieveId", achieve.DeleteAchievement)
+	}
+	// apiSemesters := router.Group("api/semesters/")
+	// apiUsers := router.Group("api/users/")
+	// apiAccounts := router.Group("api/accounts/")
 
-  // Web server serves on :8080
+	// Web server serves on :8080
 	router.Run(":8080")
 
-  // close DbSqlx when server finishes
-  defer database.CloseDb();
+	// close DbSqlx when server finishes
+	defer database.CloseDb()
 }
