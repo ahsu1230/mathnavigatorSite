af01495d1214173151b8962ee4b382b767b0a6ea
Tony Wu <tonywu315@gmail.com>
2020-02-20 16:54:26 -0500

    Created the model

--- orion/controllers/achieve/achieveModel.go
+++ orion/controllers/achieve/achieveModel.go
@@ -1,1 +1,14 @@
 package achieve
+
+import (
+	"database/sql"
+)
+
+type Achieve struct {
+	Id          uint
+	CreatedAt   uint          `db:"created_at"`
+	UpdatedAt   uint          `db:"updated_at"`
+	DeletedAt   sql.NullInt64 `db:"deleted_at"`
+	Year        uint          `json:"programId"`
+	Message     string        `json:"message"`
+}
