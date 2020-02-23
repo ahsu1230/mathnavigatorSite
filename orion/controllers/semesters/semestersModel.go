cb949938182e09833d55933adcda4b44efe0d7b9
Tony Wu <tonywu315@gmail.com>
2020-02-23 11:49:22 -0500

    Added semester model

--- orion/controllers/semesters/semestersModel.go
+++ orion/controllers/semesters/semestersModel.go
@@ -1,1 +1,14 @@
 package semesters
+
+import (
+	"database/sql"
+)
+
+type Semester struct {
+	Id         uint
+	CreatedAt  uint          `db:"created_at"`
+	UpdatedAt  uint          `db:"updated_at"`
+	DeletedAt  sql.NullInt64 `db:"deleted_at"`
+	SemesterId string        `db:"semester_id" json:"semesterId"`
+	Title      string        `json:"string"`
+}
