74d38480d4c6d0f0255c1146371f1c8a56fae7a1
Aaron Hsu <aaron@nextbit.com>
2020-02-24 23:34:03 -0500

    Complete all programRepo Tests

--- orion/repos/program_repo_test.go
+++ orion/repos/program_repo_test.go
@@ -18,14 +18,17 @@ func initTest(t *testing.T) (*sql.DB, sqlmock.Sqlmock, repos.ProgramRepoInterfac
     return db, mock, repo
 }
 
+//
+// Test Select All
+//
 func TestSelectAllPrograms(t *testing.T) {
     db, mock, repo := initTest(t)
     defer db.Close()
 
-    // Mock DB queries and execute
+    // Mock DB statements and execute
     rows := sqlmock.NewRows([]string{"Id", "CreatedAt", "UpdatedAt", "DeletedAt", "Name", "ProgramId", "Grade1", "Grade2", "Description"}).
                             AddRow(1, 1000, 1000, sql.NullInt64{}, "prog1", "Program1", 2, 3, "descript1")
-				mock.ExpectPrepare("SELECT (.+) FROM programs").ExpectQuery().WillReturnRows(rows)
+				mock.ExpectPrepare("^SELECT (.+) FROM programs").ExpectQuery().WillReturnRows(rows)
     got, err := repo.SelectAll()
     if err != nil {
         t.Errorf("Unexpected error %v", err)
@@ -48,153 +51,132 @@ func TestSelectAllPrograms(t *testing.T) {
     if !reflect.DeepEqual(got, want) {
         t.Errorf("Values not equal: got = %v, want = %v", got, want)
     }
+    if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
 }
 
+//
+// Select One
+//
+func TestSelectProgram(t *testing.T) {
+    db, mock, repo := initTest(t)
+    defer db.Close()
 
+    // Mock DB statements and execute
+    rows := sqlmock.NewRows([]string{"Id", "CreatedAt", "UpdatedAt", "DeletedAt", "Name", "ProgramId", "Grade1", "Grade2", "Description"}).
+                            AddRow(1, 1000, 1000, sql.NullInt64{}, "prog1", "Program1", 2, 3, "descript1")
+    mock.ExpectPrepare("^SELECT (.+) FROM programs WHERE program_id=?").
+        ExpectQuery().
+        WithArgs("prog1").
+        WillReturnRows(rows)
+    got, err := repo.SelectByProgramId("prog1") // Correct programId
+    if err != nil {
+        t.Errorf("Unexpected error %v", err)
+    }
 
+    // Validate results
+    want := domains.Program{
+        Id:         1,
+        CreatedAt:  1000,
+        UpdatedAt:  1000,
+        DeletedAt:  sql.NullInt64{},
+        ProgramId:  "prog1",
+        Name:       "Program1",
+        Grade1:     2,
+        Grade2:     3,
+        Description: "descript1",
+    }
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("Values not equal: got = %v, want = %v", got, want)
+    }
+    if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
 
+//
+// Create
+//
+func TestInsertProgram(t *testing.T) {
+    db, mock, repo := initTest(t)
+    defer db.Close()
 
+    // Mock DB statements and execute
+    result := sqlmock.NewResult(1, 1)
+    mock.ExpectPrepare("^INSERT INTO programs").
+        ExpectExec().
+        WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), "prog1", "Program1", 2, 3, "Descript1").
+        WillReturnResult(result)
+    program := domains.Program {
+        ProgramId: "prog1",
+        Name: "Program1",
+        Grade1: 2,
+        Grade2: 3,
+        Description: "Descript1",
+    }
+    err := repo.Insert(program)
+    if err != nil {
+        t.Errorf("Unexpected error %v", err)
+    }
 
+    // Validate results
+    if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
 
+//
+// Update
+//
+func TestUpdateProgram(t *testing.T) {
+    db, mock, repo := initTest(t)
+    defer db.Close()
 
+    // Mock DB statements and execute
+    result := sqlmock.NewResult(1, 1)
+    mock.ExpectPrepare("^UPDATE programs SET (.*) WHERE program_id=?").
+        ExpectExec().
+        WithArgs(sqlmock.AnyArg(), "prog2", "Program2", 2, 3, "Descript2", "prog1").
+        WillReturnResult(result)
+    program := domains.Program {
+        ProgramId: "prog2",
+        Name: "Program2",
+        Grade1: 2,
+        Grade2: 3,
+        Description: "Descript2",
+    }
+    err := repo.Update("prog1", program)
+    if err != nil {
+        t.Errorf("Unexpected error %v", err)
+    }
 
+    // Validate results
+    if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
 
+//
+// Delete
+//
+func TestDeleteProgram(t *testing.T) {
+    db, mock, repo := initTest(t)
+    defer db.Close()
 
+    // Mock DB statements and execute
+    result := sqlmock.NewResult(1, 1)
+    mock.ExpectPrepare("^DELETE FROM programs WHERE program_id=?").
+        ExpectExec().
+        WithArgs("prog1").
+        WillReturnResult(result)
+    err := repo.Delete("prog1")
+    if err != nil {
+        t.Errorf("Unexpected error %v", err)
+    }
 
-
-
-
-
-
-
-
-
-// func TestStoreGetAllPrograms(t *testing.T) {
-//     db, dbx, mock := initTest(t)
-// 	defer db.Close()
-
-//     // Mock expected outcome
-//     rows := sqlmock.NewRows(domains.ProgramColumns).
-//         AddRow(1, 1000, 1000, nil, "prog1", "Program1", 2, 3, "description1").
-//         AddRow(2, 2000, 2000, nil, "prog2", "Program2", 5, 6, "description2")
-//     mock.ExpectQuery("^SELECT (.+) FROM programs").WillReturnRows(rows)
-
-//     // Execute method
-// 	if _, err := store.GetAllPrograms(dbx); err != nil {
-// 		t.Errorf("Error was not expected: %s", err)
-// 	}
-
-//     // Check results
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-// }
-
-// func TestStoreGetProgramById(t *testing.T) {
-//     db, dbx, mock := initTest(t)
-//     defer db.Close()
-
-//     // Mock expected outcome with valid programId
-//     rows := sqlmock.NewRows(domains.ProgramColumns).
-//         AddRow(1, 1000, 1000, nil, "prog1", "Program1", 2, 3, "description1")
-//     mock.ExpectQuery("^SELECT (.+) FROM programs WHERE program_id=?").
-//             WithArgs("prog1").
-//             WillReturnRows(rows)
-//     // Execute command and check expectations
-//     store.GetProgramById(dbx, "prog1")
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-
-//     // Mock expected outcome with invalid programId
-//     mock.ExpectQuery("^SELECT (.+) FROM programs WHERE program_id=?").
-//             WithArgs("prog2").
-//             WillReturnError(fmt.Errorf("not found"))
-//     // Execute command and check expectations
-//     store.GetProgramById(dbx, "prog2")
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-// }
-
-// func TestStoreCreateProgram(t *testing.T) {
-//     db, dbx, mock := initTest(t)
-// 	defer db.Close()
-
-//     // Mock expected SQL statement
-//     program := domains.Program {
-//         1, 1000, 1000, sql.NullInt64{Int64:0, Valid:false}, "prog1", "Program1", 2, 3, "description1",
-//     }
-//     mock.ExpectExec("^INSERT INTO programs").
-//         WillReturnResult(sqlmock.NewResult(1, 1)) // last rowId is 1, 1 row affected
-//     // Execute command and check expectations
-//     store.InsertProgram(dbx, program)
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-
-//     // Mock expected outcome when inserting same program again
-//     // Test unique program_id
-//     mock.ExpectExec("^INSERT INTO programs").
-//         WillReturnError(fmt.Errorf("non-unique program_id"))
-//     // Execute command and check expectations
-//     if err := store.InsertProgram(dbx, program); err == nil {
-//         t.Errorf("Expected an error but was none")
-//     }
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-// }
-
-// // func TestStoreUpdateProgram(t *testing.T) {
-// //     db, dbx, mock := initTest()
-// // 	    defer db.Close()
-// //
-// //     // Mock expected outcome with valid programId
-// //     rows := sqlmock.NewRows(domains.ProgramColumns).
-// //         AddRow(1, 1000, 1000, nil, "prog1", "Program1", 2, 3, "description1")
-// //     mock.ExpectQuery("^SELECT (.+) FROM programs WHERE program_id=?").
-// //             WithArgs("prog1").
-// //             WillReturnRows(rows)
-// //     // Execute command and check expectations
-// //     store.GetProgramById(dbx, "prog1")
-// // 	if err := mock.ExpectationsWereMet(); err != nil {
-// // 		t.Errorf("There were unfulfilled expectations: %s", err)
-// // 	}
-// //
-// //     // Mock expected outcome with invalid programId
-// //     mock.ExpectQuery("^SELECT (.+) FROM programs WHERE program_id=?").
-// //             WithArgs("prog2").
-// //             WillReturnError(fmt.Errorf("not found"))
-// //     // Execute command and check expectations
-// //     store.GetProgramById(dbx, "prog2")
-// // 	if err := mock.ExpectationsWereMet(); err != nil {
-// // 		t.Errorf("There were unfulfilled expectations: %s", err)
-// // 	}
-// // }
-
-// func TestStoreDeleteProgram(t *testing.T) {
-//     db, dbx, mock := initTest(t)
-// 	defer db.Close()
-
-//     // Mock expected outcome with valid programId
-//     sqlmock.NewRows(domains.ProgramColumns).
-//         AddRow(1, 1000, 1000, nil, "prog1", "Program1", 2, 3, "description1")
-//     mock.ExpectExec("^DELETE FROM programs WHERE program_id=?").
-//             WithArgs("prog1")
-//     // Execute command and check expectations
-//     store.DeleteProgram(dbx, "prog1")
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-
-//     // Mock expected outcome with invalid programId
-//     mock.ExpectExec("^DELETE FROM programs WHERE program_id=?").
-//             WithArgs("prog1").
-//             WillReturnError(fmt.Errorf("not found"))
-//     // Execute command and check expectations
-//     store.DeleteProgram(dbx, "prog1")
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("There were unfulfilled expectations: %s", err)
-// 	}
-// }
+    // Validate results
+    if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
