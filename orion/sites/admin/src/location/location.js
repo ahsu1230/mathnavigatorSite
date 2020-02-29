4d19b2dbd275a00e98925f6cff8a15924c8756e2
cyung <mythicat554@gmail.com>
2020-02-29 15:06:19 -0500

    fixed indents and added more rows

--- orion/sites/admin/src/location/location.js
+++ orion/sites/admin/src/location/location.js
@@ -12,19 +12,31 @@ export class LocationPage extends React.Component {
             address: "123 Sesame Street",
             roomNum: 23
         }
+        var fakeLocation1 = {
+            locId: 4567,
+            address: "456 Sesame St",
+            roomNum: 892
+        }
+        var fakeLocation2 = {
+            locId: 123456,
+            address: "12345 Park Place",
+            roomNum: 212
+        }
         return (
             <div id="view-location">
                 <h1>All Locations ({numLocations})</h1>
                 <ul id="list-heading">
                     <li className="li-med">Location ID</li>
                     <li className="li-med">Address</li>
-                    <li className="li-med">Room Number</li>
+                    <li className="li-large">Room Number</li>
                 </ul>
                 <ul>
                     <LocationRow locationObj = {fakeLocation}/>
+                    <LocationRow locationObj = {fakeLocation1}/>
+                    <LocationRow locationObj = {fakeLocation2}/>
                 </ul>
-                    <button>
-                    <Link className="add-location" to={"/locations/add"}>Add Location</Link>
+                    <button id="add-location">
+                        <Link to={"/locations/add"}>Add Location</Link>
                     </button>
             </div>
         );
@@ -43,7 +55,7 @@ class LocationRow extends React.Component {
                 <li className="li-med">{address}</li>
                 <li className="li-small">{roomNum}</li>
                 <Link to={url}>Edit</Link>
-                </ul>
+            </ul>
         );
     }
 }
