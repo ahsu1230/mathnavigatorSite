91649873413f8774e82fb49bc39ebc5e56af7b74
Tony Wu <tonywu315@gmail.com>
2020-03-06 18:48:25 -0500

    Added other migrations

---
+++ orion/pkg/repos/migrations/000002_create_table_announce.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE announcements
+(
+    id         int unsigned NOT NULL AUTO_INCREMENT,
+    created_at bigint(20)   NOT NULL,
+    updated_at bigint(20)   NOT NULL,
+    deleted_at datetime,
+    posted_at  bigint(20)   NOT NULL,
+    author     varchar(255) NOT NULL,
+    message    text         NOT NULL,
+    PRIMARY KEY (id)
+) AUTO_INCREMENT = 1
+  DEFAULT CHARSET = UTF8MB4;
