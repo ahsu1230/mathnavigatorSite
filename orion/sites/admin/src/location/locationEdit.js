8f42b97b6fac63223c52703d3d71a6169da204d0
cyung <mythicat554@gmail.com>
2020-03-07 17:09:30 -0500

    indents and deleted extra styl code

--- orion/sites/admin/src/location/locationEdit.js
+++ orion/sites/admin/src/location/locationEdit.js
@@ -34,30 +34,30 @@ export class LocationEditPage extends React.Component {
         let deleteButton = <div></div>;
         return (
             <div id="view-location-edit">
-            <h2>{title}</h2>
-            <h4>Location ID</h4>
-            <input value={this.state.inputLocationId}
-                onChange={(e) => this.handleChange(e, "inputLocationId")}/>
-            <h4>Street</h4>
-            <input value={this.state.inputStreet}
-                onChange={(e) => this.handleChange(e, "inputStreet")}/>
-            <h4>City</h4>
-            <input value={this.state.inputStreet}
-                onChange={(e) => this.handleChange(e, "inputCity")}/>
-            <h4>State</h4>
-            <input value={this.state.inputStreet}
-                onChange={(e) => this.handleChange(e, "inputState")}/>
-            <h4>Zipcode</h4>
-            <input value={this.state.inputStreet}
-                onChange={(e) => this.handleChange(e, "inputZip")}/>
-            <h4>Room Number</h4>
-            <input value={this.state.inputRoomNum}
-                onChange={(e) => this.handleChange(e, "inputRoomNum")}/>
-            <div className="buttons">
-                <button className="btn-save" onClick={this.onClickSave}>Save</button>
-                <button className="btn-cancel" onClick={this.onClickCancel}>Cancel</button>
-                {deleteButton}
-            </div>
+                <h2>{title}</h2>
+                <h4>Location ID</h4>
+                <input value={this.state.inputLocationId}
+                    onChange={(e) => this.handleChange(e, "inputLocationId")}/>
+                <h4>Street</h4>
+                <input value={this.state.inputStreet}
+                    onChange={(e) => this.handleChange(e, "inputStreet")}/>
+                <h4>City</h4>
+                <input value={this.state.inputStreet}
+                    onChange={(e) => this.handleChange(e, "inputCity")}/>
+                <h4>State</h4>
+                <input value={this.state.inputStreet}
+                    onChange={(e) => this.handleChange(e, "inputState")}/>
+                <h4>Zipcode</h4>
+                <input value={this.state.inputStreet}
+                    onChange={(e) => this.handleChange(e, "inputZip")}/>
+                <h4>Room Number</h4>
+                <input value={this.state.inputRoomNum}
+                    onChange={(e) => this.handleChange(e, "inputRoomNum")}/>
+                <div className="buttons">
+                    <button className="btn-save" onClick={this.onClickSave}>Save</button>
+                    <button className="btn-cancel" onClick={this.onClickCancel}>Cancel</button>
+                    {deleteButton}
+                </div>
             </div>
         );
     }
