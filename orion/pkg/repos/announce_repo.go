48cc9439d8cde8224aad2d6c6d2da07bf2c86d18
mzhang2021 <mzhangschool2@gmail.com>
2020-03-07 14:02:06 -0500

    repo + controller

---
+++ orion/pkg/repos/announce_repo.go
@@ -0,0 +1,150 @@
+package repos
+
+import (
+	"database/sql"
+	"time"
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/domains"
+)
+
+type announceRepo struct {
+	db *sql.DB;
+}
+
+type AnnounceRepoInterface interface {
+	Initialize(db *sql.DB)
+	SelectAll() ([]domains.Announce, error)
+	SelectById(uint) (domains.Announce, error)
+	Insert(domains.Announce) error
+	Update(uint, domains.Announce) error
+	Delete(uint) error
+}
+
+func (ar *announceRepo) Initialize(db *sql.DB) {
+	ar.db = db
+}
+
+func (ar *announceRepo) SelectAll() ([]domains.Announce, error) {
+	results := make([]domains.Announce, 0)
+	
+	stmt, err := ar.db.Prepare("SELECT * FROM announcements")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	
+	for rows.Next() {
+		var announce domains.Announce
+		if errScan := rows.Scan(
+			&announce.Id,
+			&announce.CreatedAt,
+			&announce.UpdatedAt,
+			&announce.DeletedAt,
+			&announce.PostedAt,
+			&announce.Author,
+			&announce.Message); errScan != nil {
+			return results, errScan
+		}
+		results = append(results, announce)
+	}
+	
+	return results, nil
+}
+
+func (ar *announceRepo) SelectById(id uint) (domains.Announce, error) {
+	stmt, err := ar.db.Prepare("SELECT * FROM announcements WHERE id=?")
+	if err != nil {
+		return domains.Announce{}, err
+	}
+	defer stmt.Close()
+	
+	var announce domains.Announce
+	row := stmt.QueryRow(id)
+	errScan := row.Scan(
+		&announce.Id,
+		&announce.CreatedAt,
+		&announce.UpdatedAt,
+		&announce.DeletedAt,
+		&announce.PostedAt,
+		&announce.Author,
+		&announce.Message)
+	
+	return announce, errScan
+}
+
+func (ar *announceRepo) Insert(announce domains.Announce) error {
+	stmt, err := ar.db.Prepare("INSERT INTO announcements (" +
+								"created_at, " +
+								"updated_at, " +
+								"posted_at, " +
+								"author, " +
+								"message" +
+								") VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	
+	now := time.Now().UTC()
+	result, err := stmt.Exec(
+		now,
+		now,
+		announce.PostedAt,
+		announce.Author,
+		announce.Message)
+	if err != nil {
+		return err
+	}
+	
+	return handleSqlExecResult(result, 1, "announcement was not inserted")
+}
+
+func (ar *announceRepo) Update(id uint, announce domains.Announce) error {
+	stmt, err := ar.db.Prepare("UPDATE announcements SET " +
+								"updated_at=?, " +
+								"posted_at=?, " +
+								"author=?, " +
+								"message=? " +
+								"WHERE id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	
+	now := time.Now().UTC()
+	result, err := stmt.Exec(
+		now,
+		announce.PostedAt,
+		announce.Author,
+		announce.Message)
+	if err != nil {
+		return err
+	}
+	
+	return handleSqlExecResult(result, 1, "announcement was not updated")
+}
+
+func (ar *announceRepo) Delete(id uint) error {
+	stmt, err := ar.db.Prepare("DELETE FROM announcements WHERE id=?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	
+	result, err := stmt.Exec(id)
+	if err != nil {
+		return err
+	}
+	
+	return handleSqlExecResult(result, 1, "announcement was not deleted")
+}
+
+func CreateTestAnnounceRepo(db *sql.DB) AnnounceRepoInterface {
+	ar := &announceRepo{}
+	ar.Initialize(db)
+	return ar
+}
---
+++ orion/pkg/repos/announce_repo_test.go
@@ -0,0 +1,149 @@
+package repos
+
+import (
+	"database/sql"
+	"reflect"
+    "testing"
+    "time"
+    sqlmock "github.com/DATA-DOG/go-sqlmock"
+    "github.com/ahsu1230/mathnavigatorSite/orion/pkg/domains"
+)
+
+func initTest(t *testing.T) (*sql.DB, sqlmock.Sqlmock, AnnounceRepoInterface) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	repo := CreateTestAnnounceRepo(db)
+	return db, mock, repo
+}
+
+func TestSelectAllAnnouncements(t *testing.T) {
+	db, mock, repo := initTest(t)
+	defer db.Close()
+	
+	// Mock DB statements and execute
+	now := time.Now().UTC()
+	rows := sqlmock.NewRows([]string{"Id", "CreatedAt", "UpdatedAt", "DeletedAt", "PostedAt", "Author", "Message"}).AddRow(1, now, now, sql.NullTime{}, now, "Author Name", "Valid Message")
+	mock.ExpectPrepare("^SELECT (.+) FROM announcements").ExpectQuery().WillReturnRows(rows)
+	got, err := repo.SelectAll()
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	
+	// Validate results
+	want := []domains.Announce {
+		{
+			Id:			1,
+			CreatedAt:	now,
+			UpdatedAt:	now,
+			DeletedAt:	sql.NullTime{},
+			PostedAt:	now,
+			Author:		"Author Name",
+			Message:	"Valid Message",
+		},
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Values not equal: got = %v, want = %v", got, want)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
+
+func TestSelectAnnouncement(t *testing.T) {
+	db, mock, repo := initTest(t)
+	defer db.Close()
+	
+	// Mock DB statements and execute
+	now := time.Now().UTC()
+	rows := sqlmock.NewRows([]string{"Id", "CreatedAt", "UpdatedAt", "DeletedAt", "PostedAt", "Author", "Message"}).AddRow(1, now, now, sql.NullTime{}, now, "Author Name", "Valid Message")
+	mock.ExpectPrepare("^SELECT (.+) FROM announcements WHERE id=?").ExpectQuery().WithArgs(1).WillReturnRows(rows)
+	got, err := repo.SelectById(1)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	
+	// Validate results
+	want := domains.Announce {
+		Id:			1,
+		CreatedAt:	now,
+		UpdatedAt:	now,
+		DeletedAt:	sql.NullTime{},
+		PostedAt:	now,
+		Author:		"Author Name",
+		Message:	"Valid Message",
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("Values not equal: got = %v, want = %v", got, want)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
+
+func TestInsertAnnouncement(t *testing.T) {
+	db, mock, repo := initTest(t)
+	defer db.Close()
+	
+	// Mock DB statements and execute
+	now := time.Now().UTC()
+	result := sqlmock.NewResult(1, 1)
+	mock.ExpectPrepare("^INSERT INTO announcements").ExpectExec().WithArgs(sqlmock.AnyArg(), sqlmock.AnyArg(), now, "Author Name", "Valid Message").WillReturnResult(result)
+	announce := domains.Announce {
+		PostedAt:	now,
+		Author:		"Author Name",
+		Message:	"Valid Message",
+	}
+	err := repo.Insert(announce)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	
+	// Validate results
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
+
+func TestUpdateAnnouncement(t *testing.T) {
+	db, mock, repo := initTest(t)
+	defer db.Close()
+	
+	// Mock DB statements and execute
+	now := time.Now().UTC()
+	result := sqlmock.NewResult(1, 1)
+	mock.ExpectPrepare("^UPDATE announcements SET (.*) WHERE id=?").ExpectExec().WithArgs(sqlmock.AnyArg(), now, "Author Name", "Valid Message").WillReturnResult(result)
+	announce := domains.Announce {
+		PostedAt:	now,
+		Author:		"Author Name",
+		Message:	"Valid Message",
+	}
+	err := repo.Update(1, announce)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	
+	// Validate results
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
+
+func TestDeleteProgram(t *testing.T) {
+	db, mock, repo := initTest(t)
+	defer db.Close()
+	
+	// Mock DB statements and execute
+	result := sqlmock.NewResult(1, 1)
+	mock.ExpectPrepare("^DELETE FROM announcements WHERE id=?").ExpectExec().WithArgs(1).WillReturnResult(result)
+	err := repo.Delete(1)
+	if err != nil {
+		t.Errorf("Unexpected error %v", err)
+	}
+	
+	// Validate results
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Unfulfilled expectations: %s", err)
+	}
+}
