003af1b44dcb6f0fe470079a032e1ffbb9f933c2
mzhang2021 <mzhangschool2@gmail.com>
2020-03-07 14:09:12 -0500

    fix circleCI fail

---
+++ orion/pkg/domains/announce_valid_test.go
@@ -0,0 +1,68 @@
+package domains
+
+import (
+	"testing"
+)
+
+func TestValidAuthor(t *testing.T) {
+	// Check for valid authors
+	announce := Announce{Author: "Valid Author", Message: "not blank"}
+	if err := announce.Validate(); err != nil {
+		t.Errorf("Check was incorrect, got: %s, expected: nil", err.Error())
+	}
+
+	announce.Author = "legitimate author name"
+	if err := announce.Validate(); err != nil {
+		t.Errorf("Check was incorrect, got: %s, expected: nil", err.Error())
+	}
+
+	// Check for invalid authors
+	announce.Author = "&&"
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid author")
+	}
+	
+	announce.Author = "  @1 + 4"
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid author")
+	}
+	
+	announce.Author = ""
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid author")
+	}
+}
+	
+func TestValidMessage(t *testing.T) {
+	// Check for valid messages
+	announce := Announce{Author: "Valid Author", Message: "not blank"}
+	if err := announce.Validate(); err != nil {
+		t.Errorf("Check was incorrect, got: %s, expected: nil", err.Error())
+	}
+	
+	announce.Message = "                 a   "
+	if err := announce.Validate(); err != nil {
+		t.Errorf("Check was incorrect, got: %s, expected: nil", err.Error())
+	}
+	
+	announce.Message = "I'm a filled message with speci@l ch@r&cterS"
+	if err := announce.Validate(); err != nil {
+	t.Errorf("Check was incorrect, got: %s, expected: nil", err.Error())
+	}
+	
+	// Check for invalid messages
+	announce.Message = ""
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid message")
+	}
+	
+	announce.Message = "                                                    "
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid message")
+	}
+	
+	announce.Message = "     123%                                           "
+	if err := announce.Validate(); err == nil {
+		t.Error("Check was incorrect, got: nil, expected: invalid message")
+	}
+}
