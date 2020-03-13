d4d5a7ed33c7c266874ce6e04f1306e72d8876b4
Tony Wu <tonywu315@gmail.com>
2020-03-13 19:54:56 -0400

    Fixed sql helper

--- orion/pkg/sql_helper/sql.go
+++ orion/pkg/sql_helper/sql.go
@@ -5,21 +5,9 @@ import (
 )
 
 // NullUint represents an uint that may be null.
-// NullUint implements the Scanner interface so
-// it can be used as a scan destination, similar to NullString.
 type NullUint struct {
-	Uint uint
+	Uint  uint
 	Valid bool // Valid is true if Uint is not NULL
-}
-
-// Scan implements the Scanner interface.
-func (n *NullUint) Scan(value interface{}) error {
-	if value == nil {
-		n.Uint, n.Valid = 0, false
-		return nil
-	}
-	n.Valid = true
-	return convertAssign(&n.Uint, value)
 }
 
 // Value implements the driver Valuer interface.
