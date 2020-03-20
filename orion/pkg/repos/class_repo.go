f797428658ace3b70d45363ccecf86f88563fdc7
Tony Wu <tonywu315@gmail.com>
2020-03-20 17:35:21 -0400

    Added class repo

--- orion/pkg/repos/class_repo.go
+++ orion/pkg/repos/class_repo.go
@@ -1,1 +1,195 @@
 package repos
+
+import (
+	"database/sql"
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/domains"
+	"time"
+)
+
+// Global variable
+var ClassRepo ClassRepoInterface = &classRepo{}
+
+// Implements interface classRepoInterface
+type classRepo struct {
+	db *sql.DB // golang native db connection
+}
+
+// Interface to implement
+type ClassRepoInterface interface {
+	Initialize(db *sql.DB)
+	SelectAll() ([]domains.Class, error)
+	SelectByClassId(string) (domains.Class, error)
+	Insert(domains.Class, domains.Program, domains.Semester, domains.Location) error
+	Update(string, domains.Class, domains.Program, domains.Semester, domains.Location) error
+	Delete(string) error
+}
+
+func (cr *classRepo) Initialize(db *sql.DB) {
+	cr.db = db
+}
+
+func (cr *classRepo) SelectAll() ([]domains.Class, error) {
+	results := make([]domains.Class, 0)
+
+	stmt, err := cr.db.Prepare("SELECT * FROM classes")
+	if err != nil {
+		return nil, err
+	}
+	defer stmt.Close()
+	rows, err := stmt.Query()
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var class domains.Class
+		if errScan := rows.Scan(
+			&class.Id,
+			&class.CreatedAt,
+			&class.UpdatedAt,
+			&class.DeletedAt,
+			&class.ProgramId,
+			&class.SemesterId,
+			&class.ClassKey,
+			&class.ClassId,
+			&class.LocationId,
+			&class.Times,
+			&class.StartDate,
+			&class.EndDate); errScan != nil {
+			return results, errScan
+		}
+		results = append(results, class)
+	}
+	return results, nil
+}
+
+func (cr *classRepo) SelectByClassId(classId string) (domains.Class, error) {
+	statement := "SELECT * FROM classes WHERE class_id=?"
+	stmt, err := cr.db.Prepare(statement)
+	if err != nil {
+		return domains.Class{}, err
+	}
+	defer stmt.Close()
+
+	var class domains.Class
+	row := stmt.QueryRow(classId)
+	errScan := row.Scan(
+		&class.Id,
+		&class.CreatedAt,
+		&class.UpdatedAt,
+		&class.DeletedAt,
+		&class.ProgramId,
+		&class.SemesterId,
+		&class.ClassKey,
+		&class.ClassId,
+		&class.LocationId,
+		&class.Times,
+		&class.StartDate,
+		&class.EndDate)
+	return class, errScan
+}
+
+func (cr *classRepo) Insert(class domains.Class, program domains.Program, semester domains.Semester, location domains.Location) error {
+	statement := "INSERT INTO classes (" +
+		"created_at, " +
+		"updated_at, " +
+		"program_id, " +
+		"semester_id, " +
+		"class_key, " +
+		"class_id, " +
+		"location_id, " +
+		"times, " +
+		"start_date, " +
+		"end_date, " +
+		") VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+	stmt, err := cr.db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	now := time.Now().UTC()
+	execResult, err := stmt.Exec(
+		now,
+		now,
+		program.ProgramId,
+		semester.SemesterId,
+		class.ClassKey,
+		generateClassId(program, semester, class),
+		location.LocId,
+		class.Times,
+		class.StartDate,
+		class.EndDate)
+	if err != nil {
+		return err
+	}
+	return handleSqlExecResult(execResult, 1, "class was not inserted")
+}
+
+func (cr *classRepo) Update(classId string, class domains.Class, program domains.Program, semester domains.Semester, location domains.Location) error {
+	statement := "UPDATE classes SET " +
+		"updated_at, " +
+		"program_id, " +
+		"semester_id, " +
+		"class_key, " +
+		"class_id, " +
+		"location_id, " +
+		"times, " +
+		"start_date, " +
+		"end_date, " +
+		"WHERE class_id=?"
+	stmt, err := cr.db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	now := time.Now().UTC()
+	execResult, err := stmt.Exec(
+		now,
+		program.ProgramId,
+		semester.SemesterId,
+		class.ClassKey,
+		generateClassId(program, semester, class),
+		location.LocId,
+		class.Times,
+		class.StartDate,
+		class.EndDate,
+		classId)
+	if err != nil {
+		return err
+	}
+	return handleSqlExecResult(execResult, 1, "class was not updated")
+}
+
+func (cr *classRepo) Delete(classId string) error {
+	statement := "DELETE FROM classes WHERE class_id=?"
+	stmt, err := cr.db.Prepare(statement)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	execResult, err := stmt.Exec(classId)
+	if err != nil {
+		return err
+	}
+	return handleSqlExecResult(execResult, 1, "class was not deleted")
+}
+
+// For Tests Only
+func CreateTestClassRepo(db *sql.DB) ClassRepoInterface {
+	cr := &classRepo{}
+	cr.Initialize(db)
+	return cr
+}
+
+func generateClassId(program domains.Program, semester domains.Semester, class domains.Class) string {
+	classId := program.ProgramId + "_" + semester.SemesterId + "_"
+	if class.ClassKey.Valid {
+		return classId + "_" + class.ClassKey.String
+	}
+	return classId
+}
