9101dfa90a34527381473094d3cb6861912c918f
Tony Wu <tonywu315@gmail.com>
2020-03-20 19:08:28 -0400

    Added users controller

--- orion/pkg/controllers/user_controller.go
+++ orion/pkg/controllers/user_controller.go
@@ -1,1 +1,78 @@
 package controllers
+
+import (
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/domains"
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetAllUsers(c *gin.Context) {
+	userList, err := services.UserService.GetAll()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, userList)
+	}
+}
+
+func GetUserById(c *gin.Context) {
+	// Incoming parameters
+	id := ParseParamId(c)
+
+	user, err := services.UserService.GetById(id)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+
+func CreateUser(c *gin.Context) {
+	// Incoming JSON
+	var userJson domains.User
+	c.BindJSON(&userJson)
+
+	if err := userJson.Validate(); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := services.UserService.Create(userJson)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+}
+
+func UpdateUser(c *gin.Context) {
+	// Incoming JSON & Parameters
+	id := ParseParamId(c)
+	var userJson domains.User
+	c.BindJSON(&userJson)
+
+	if err := userJson.Validate(); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := services.UserService.Update(id, userJson)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+}
+
+func DeleteUser(c *gin.Context) {
+	// Incoming Parameters
+	id := ParseParamId(c)
+
+	err := services.UserService.Delete(id)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.Status(http.StatusOK)
+	}
+}
