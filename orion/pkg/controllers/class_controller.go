890a330d429b77818887c58e33d144dd0bc8e727
Tony Wu <tonywu315@gmail.com>
2020-03-21 12:50:23 -0400

    Added class controller

--- orion/pkg/controllers/class_controller.go
+++ orion/pkg/controllers/class_controller.go
@@ -1,1 +1,83 @@
 package controllers
+
+import (
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/domains"
+	"github.com/ahsu1230/mathnavigatorSite/orion/pkg/services"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetAllClasses(c *gin.Context) {
+	classList, err := services.ClassService.GetAll()
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, classList)
+	}
+	return
+}
+
+func GetClassById(c *gin.Context) {
+	// Incoming parameters
+	classId := c.Param("classId")
+
+	class, err := services.ClassService.GetByClassId(classId)
+	if err != nil {
+		c.String(http.StatusNotFound, err.Error())
+	} else {
+		c.JSON(http.StatusOK, class)
+	}
+	return
+}
+
+func CreateClass(c *gin.Context) {
+	// Incoming JSON
+	var classJson domains.Class
+	c.BindJSON(&classJson)
+
+	if err := classJson.Validate(); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := services.ClassService.Create(classJson)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+	return
+}
+
+func UpdateClass(c *gin.Context) {
+	// Incoming JSON & Parameters
+	classId := c.Param("classId")
+	var classJson domains.Class
+	c.BindJSON(&classJson)
+
+	if err := classJson.Validate(); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	err := services.ClassService.Update(classId, classJson)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+	return
+}
+
+func DeleteClass(c *gin.Context) {
+	// Incoming Parameters
+	classId := c.Param("classId")
+
+	err := services.ClassService.Delete(classId)
+	if err != nil {
+		c.String(http.StatusInternalServerError, err.Error())
+	} else {
+		c.Status(http.StatusOK)
+	}
+	return
+}
