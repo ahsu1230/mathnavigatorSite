49c9c58b482e24097bac470988a7ea574231f2ac
cyung <mythicat554@gmail.com>
2020-03-31 00:06:38 -0400

    made overall page

--- orion/sites/admin/src/home/home.js
+++ orion/sites/admin/src/home/home.js
@@ -5,9 +5,21 @@ import ReactDOM from 'react-dom';
 
 export class HomePage extends React.Component {
 	render() {
+		const unpubContent = 5
 		return (
-      <div id="view-home">
-      </div>
+	      <div id="view-home">
+		      <h2> Unpublished Content </h2>
+			  	  <ul>
+				      {unpubContent}
+				  </ul>
+			  <h2> Registrations </h2>
+			      <ul> New Users </ul>
+				  <ul> Questions </ul>
+				  <ul> Complaints </ul>
+			  <button id="go-to-page">
+			  	  Add Location
+			  </button>
+	      </div>
 		);
 	}
 }
