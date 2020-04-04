aaf6f77237a31225ef6009c19c7d405fc0c02408
cyung <mythicat554@gmail.com>
2020-04-04 15:15:26 -0400

    rearranged box

--- orion/sites/admin/src/home/home.js
+++ orion/sites/admin/src/home/home.js
@@ -8,23 +8,30 @@ export class HomePage extends React.Component {
 		const unpubContent = 5
 		return (
 	      <div id="view-home">
-		  	  <div>
-			      <h2> Unpublished Content </h2>
-				  	  <ul>
-					      {unpubContent}
-					  </ul>
-				  <h2> Registrations </h2>
-				      <ul> New Users </ul>
-					  <ul> Questions </ul>
-					  <ul> Complaints </ul>
+		  	  <div id="view-content">
+				  <div>
+				      <h2> Unpublished Content </h2>
+					  	  <ul>
+						      {unpubContent}
+						  </ul>
+					  <h2> Registrations </h2>
+					      <ul> New Users </ul>
+						  <ul> Questions </ul>
+						  <ul> Complaints </ul>
+				 </div>
+			  	 <div>
+				  	<div className="boxed">
+				 	text
+					</div>
+			  	 </div>
 			 </div>
-		  	 <div className="boxed">
-			 	text
-		  	</div>
-			<button id="go-to-page">
-				Go to Page
-			</button>
+			 <div id="view-button">
+				 <button id="go-to-page">
+					Go to Page
+				 </button>
+			 </div>
 		  </div>
+
 
 		);
 	}
