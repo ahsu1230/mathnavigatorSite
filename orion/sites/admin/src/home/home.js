07553efd55c3489d54bbcec16f96b06131b55836
cyung <mythicat554@gmail.com>
2020-04-04 15:22:48 -0400

    worked on sorting divs

--- orion/sites/admin/src/home/home.js
+++ orion/sites/admin/src/home/home.js
@@ -9,22 +9,19 @@ export class HomePage extends React.Component {
 		return (
 	      <div id="view-home">
 		  	  <div id="view-content">
-				  <div>
-				      <h2> Unpublished Content </h2>
-					  	  <ul>
-						      {unpubContent}
-						  </ul>
-					  <h2> Registrations </h2>
-					      <ul> New Users </ul>
-						  <ul> Questions </ul>
-						  <ul> Complaints </ul>
-				 </div>
-			  	 <div>
-				  	<div className="boxed">
+			      <h2> Unpublished Content </h2>
+				  	  <ul>
+					      {unpubContent}
+					  </ul>
+				  <h2> Registrations </h2>
+				      <ul> New Users </ul>
+					  <ul> Questions </ul>
+					  <ul> Complaints </ul>
+				  <div className="boxed">
 				 	text
-					</div>
-			  	 </div>
-			 </div>
+				  </div>
+			  </div>
+		  </div>
 			 <div id="view-button">
 				 <button id="go-to-page">
 					Go to Page
