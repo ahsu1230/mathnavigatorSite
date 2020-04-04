e70eafb1dbc8356d6b10e86e2d3b206cd22495a6
cyung <mythicat554@gmail.com>
2020-04-04 14:44:09 -0400

    overall layout for program page with checkboxes, publish button, etc

--- orion/sites/admin/src/programs/program.js
+++ orion/sites/admin/src/programs/program.js
@@ -22,25 +22,38 @@ export class ProgramPage extends React.Component {
       });
   }
 
-	render() {
+  onClickSelectAll() {
+      var items = document.getElementsByName("unpublished");
+      for (var i=0; i<items.length; i++) {
+          if (items[i].type == 'checkbox') {
+              items[i].checked = true;
+          }
+      }
+  }
+
+  render() {
     const rows = this.state.list.map((row, index) => {
       return <ProgramRow key={index} row={row}/>
     });
     const numRows = rows.length;
-		return (
+    let numUnpublished = 0;
+    let numSelected = 0;
+	return (
       <div id="view-program">
         <ul>
             <h1>All Programs ({numRows}) </h1>
-            <div> You have 5 unpublished items. </div>
-            <div> You have selected 0 items to publish. </div>
+            <div> You have {numUnpublished} unpublished items. </div>
+            <div> You have selected {numSelected} items to publish. </div>
         </ul>
         <ul id="list-heading">
+          <button className="li-small" onClick={this.onClickSelectAll}>Select All</button>
           <li className="li-med">ProgramKey</li>
           <li className="li-med">Name</li>
           <li className="li-small">Grade1</li>
           <li className="li-small">Grade2</li>
         </ul>
         <ul id="list-rows">
+            <script>hideInputs()</script>
           {rows}
         </ul>
         <ul id="list-buttons">
@@ -64,9 +77,16 @@ class ProgramRow extends React.Component {
   render() {
     const row = this.props.row;
     const url = "/program/" + row.programId + "/edit";
+    let isUnpublished = true;
+    let checkbox = <div></div>;
+    if (isUnpublished){
+        checkbox = (<input className="li-small" type="checkbox" name="unpublished" onClick={this.onClickBox}/>);
+    } else {
+        checkbox = (<div className="li-small"></div>);
+    }
     return (
       <li className="program-row">
-        <input type="checkbox" name="unpublished"/>
+        {checkbox}
         <div className="li-med">{row.programId}</div>
         <div className="li-med">{row.name}</div>
         <div className="li-small">{row.grade1}</div>
