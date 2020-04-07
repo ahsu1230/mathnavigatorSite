38eb38c859f45234c7275ae476b6266ebc3fbba2
cyung <mythicat554@gmail.com>
2020-04-06 23:32:43 -0400

    fixed more merge conflicts

--- orion/sites/admin/src/semesters/semester.js
+++ orion/sites/admin/src/semesters/semester.js
@@ -4,8 +4,6 @@ import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
 export class SemesterPage extends React.Component {
-<<<<<<< HEAD
-<<<<<<< HEAD
   constructor(props) {
     super(props);
     this.state = {
@@ -37,55 +35,6 @@ export class SemesterPage extends React.Component {
 }
 
 class SemesterRow extends React.Component {
-  render() {
-    const semesterId = this.props.semesterObj.semesterId;
-    const title = this.props.semesterObj.title;
-    const url = "/semester/" + "/edit";
-    return (
-      <ul className="semester-lists">
-        <li className="li-large"> {semesterId} </li>
-        <li className="li-large"> {title} </li>
-        <Link to={url}> Edit </Link>
-      </ul>
-    );
-  }
-=======
-=======
->>>>>>> c15f24dc4318ffae807d39aef3ef62f1b6948b26
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [],
-        };
-    }
-
-    render() {
-        const rows = this.state.list.map((row, index) => {
-            return <SemesterRow key={index} row={row} />;
-        });
-        const numRows = rows.length;
-        return (
-            <div id="view-semester">
-                <h1>All Semesters ({numRows}) </h1>
-                <ul className="semester-lists">
-                    <li className="li-large">SemesterID</li>
-                    <li className="li-large">Title</li>
-                    <li className="li-small"> </li>
-                </ul>
-                <ul id="view-semester">{rows}</ul>
-                <Link to={"/semesters/add"}>
-                    {" "}
-                    <button className="semester-button">
-                        {" "}
-                        Add Semester
-                    </button>{" "}
-                </Link>
-            </div>
-        );
-    }
-}
-
-class SemesterRow extends React.Component {
     render() {
         const semesterId = this.props.semesterObj.semesterId;
         const title = this.props.semesterObj.title;
@@ -98,8 +47,4 @@ class SemesterRow extends React.Component {
             </ul>
         );
     }
-<<<<<<< HEAD
->>>>>>> a27fb3b5070f8e1928daed628fb9a9038d1e89b9
-=======
->>>>>>> c15f24dc4318ffae807d39aef3ef62f1b6948b26
 }
--- orion/sites/home/src/index.js
+++ orion/sites/home/src/index.js
@@ -9,30 +9,6 @@ import { HomePage } from "./home/home.js";
 const Home = () => <HomePage />;
 
 class AppContainer extends React.Component {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  render() {
-    return (
-      <Router>
-        <AppWithRouter />
-      </Router>
-    );
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Switch>
-          <Route path="/" exact component={Home} />
-        </Switch>
-      </div>
-    );
-  }
-=======
-=======
->>>>>>> c15f24dc4318ffae807d39aef3ef62f1b6948b26
     render() {
         return (
             <Router>
@@ -52,10 +28,6 @@ class App extends React.Component {
             </div>
         );
     }
-<<<<<<< HEAD
->>>>>>> a27fb3b5070f8e1928daed628fb9a9038d1e89b9
-=======
->>>>>>> c15f24dc4318ffae807d39aef3ef62f1b6948b26
 }
 
 const AppWithRouter = withRouter(App);
