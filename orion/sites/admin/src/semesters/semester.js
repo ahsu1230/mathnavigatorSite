e81f2ac409774155fd5a0af0b6401bede7eb940c
cyung <mythicat554@gmail.com>
2020-04-06 23:35:05 -0400

    fixed spaces

--- orion/sites/admin/src/semesters/semester.js
+++ orion/sites/admin/src/semesters/semester.js
@@ -4,34 +4,37 @@ import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
 export class SemesterPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+        };
+    }
 
-  render() {
-    const rows = this.state.list.map((row, index) => {
-      return <SemesterRow key={index} row={row} />;
-    });
-    const numRows = rows.length;
-    return (
-      <div id="view-semester">
-        <h1>All Semesters ({numRows}) </h1>
-        <ul className="semester-lists">
-          <li className="li-large">SemesterID</li>
-          <li className="li-large">Title</li>
-          <li className="li-small"> </li>
-        </ul>
-        <ul id="view-semester">{rows}</ul>
-        <Link to={"/semesters/add"}>
-          {" "}
-          <button className="semester-button"> Add Semester</button>{" "}
-        </Link>
-      </div>
-    );
-  }
+    render() {
+        const rows = this.state.list.map((row, index) => {
+            return <SemesterRow key={index} row={row} />;
+        });
+        const numRows = rows.length;
+        return (
+            <div id="view-semester">
+                <h1>All Semesters ({numRows}) </h1>
+                <ul className="semester-lists">
+                    <li className="li-large">SemesterID</li>
+                    <li className="li-large">Title</li>
+                    <li className="li-small"> </li>
+                </ul>
+                <ul id="view-semester">{rows}</ul>
+                <Link to={"/semesters/add"}>
+                    {" "}
+                    <button className="semester-button">
+                        {" "}
+                        Add Semester
+                    </button>{" "}
+                </Link>
+            </div>
+        );
+    }
 }
 
 class SemesterRow extends React.Component {
