c2f6ae5a54082f8cca7557b4f1ed5ac677d2304d
cyung <mythicat554@gmail.com>
2020-04-11 02:25:25 -0400

    added checkboxes, messages, publish button, and checkall

--- orion/sites/admin/src/location/location.js
+++ orion/sites/admin/src/location/location.js
@@ -20,29 +20,79 @@ export class LocationPage extends React.Component {
         });
     }
 
+    onClickSelectAll() {
+        var items = document.getElementsByName("unpublished");
+        for (var i = 0; i < items.length; i++) {
+            if (items[i].type == "checkbox") {
+                items[i].checked = true;
+            }
+        }
+    }
+
+    onClickPublish() {
+        console.log("clicked publish");
+    }
+
     render() {
         const location = this.state.list.map((location, index) => {
             return <LocationRow key={index} location={location} />;
         });
         const numLocations = location.length;
+        let numUnpublished = 0;
+        let numSelected = 0;
         return (
             <div id="view-location">
-                <h1>All Locations ({numLocations})</h1>
+                <div>
+                    <h1>All Locations ({numLocations})</h1>
+                    <p>
+                        You have {numUnpublished} unpublished items. <br/>
+                        You have selected {numSelected} items to publish.
+                    </p>
+                </div>
                 <ul id="list-heading">
+                    <button
+                        className="li-small"
+                        onClick={this.onClickSelectAll}>
+                        Select All
+                    </button>
                     <li className="li-med">Location ID</li>
                     <li className="li-large">Address</li>
                     <li className="li-large">Room</li>
                 </ul>
                 <ul>{location}</ul>
-                <button id="add-location">
-                    <Link to={"/locations/add"}>Add Location</Link>
-                </button>
+                <div id="list-buttons">
+                    <button>
+                        <Link to={"/locations/add"} id="add-location">Add Location</Link>
+                    </button>
+                    <button
+                        id="publish"
+                        className="publish"
+                        onClick={this.onClickPublish}>
+                        Publish
+                    </button>
+                </div>
             </div>
         );
     }
 }
 
 class LocationRow extends React.Component {
+    renderCheckbox(isUnpublished) {
+        let checkbox = <div> </div>;
+        if (isUnpublished) {
+            return (checkbox = (
+                <input
+                    className="li-small"
+                    type="checkbox"
+                    name="unpublished"
+                    onClick={this.onClickBox}
+                />
+            ));
+        } else {
+            return (checkbox = <div className="li-small"></div>);
+        }
+    }
+
     render() {
         const locId = this.props.location.locId;
         const address1 = this.props.location.street;
@@ -54,8 +104,10 @@ class LocationRow extends React.Component {
             this.props.location.zipcode;
         const room = this.props.location.room;
         const url = "/locations/" + locId + "/edit";
+        let checkbox = this.renderCheckbox(true);
         return (
             <ul id="location-row">
+                {checkbox}
                 <li className="li-med">{locId}</li>
                 <li className="li-large">
                     <div> {address1} </div>
