c929d8b43652d958309f07316354845a3937e3d9
cyung <mythicat554@gmail.com>
2020-04-13 14:17:53 -0400

    ran prettify

--- orion/sites/admin/src/home/home.js
+++ orion/sites/admin/src/home/home.js
@@ -4,32 +4,28 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export class HomePage extends React.Component {
-	render() {
-		return (
-	      <div id="view-home">
-		  	  <div id="view-content">
-			      <h2 id="unpublished-heading"> Unpublished Content </h2>
-				  	  <ul>
-					      <li>Programs</li>
-						  <li>Locations</li>
-						  <li>Achievements</li>
-					  </ul>
-				  <h2> Registrations </h2>
-				      <ul>
-					  	  <li>New Users</li>
-						  <li>Questions</li>
-						  <li>Complaints</li>
-					  </ul>
-			  </div>
-			  <div id="box-and-button">
-				  <div className="boxed">
-					text
-				 </div>
-				 <button id="go-to-page">
-					Go to Page
-				 </button>
-			 </div>
-		  </div>
-		);
-	}
+    render() {
+        return (
+            <div id="view-home">
+                <div id="view-content">
+                    <h2 id="unpublished-heading"> Unpublished Content </h2>
+                    <ul>
+                        <li>Programs</li>
+                        <li>Locations</li>
+                        <li>Achievements</li>
+                    </ul>
+                    <h2> Registrations </h2>
+                    <ul>
+                        <li>New Users</li>
+                        <li>Questions</li>
+                        <li>Complaints</li>
+                    </ul>
+                </div>
+                <div id="box-and-button">
+                    <div className="boxed">text</div>
+                    <button id="go-to-page">Go to Page</button>
+                </div>
+            </div>
+        );
+    }
 }
