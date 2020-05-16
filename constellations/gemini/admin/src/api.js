457de690b7a77730938aa0e21c6b2dad0ab44131
Aaron Hsu <aaron@nextbit.com>
2020-05-16 17:57:15 -0400

    fix prettify

--- constellations/gemini/admin/src/api.js
+++ constellations/gemini/admin/src/api.js
@@ -1,7 +1,8 @@
 "use strict";
 import axios from "axios";
 
-var BASE_HOST = process.env.NODE_ENV == "production" ? "" : "http://localhost:6001";
+var BASE_HOST =
+    process.env.NODE_ENV == "production" ? "" : "http://localhost:6001";
 
 export default axios.create({
     baseURL: BASE_HOST,
--- constellations/gemini/user/src/api.js
+++ constellations/gemini/user/src/api.js
@@ -1,7 +1,8 @@
 "use strict";
 import axios from "axios";
 
-const orionBaseUrl = process.env.REACT_APP_ORION_HOST || "http://localhost:6001";
+const orionBaseUrl =
+    process.env.REACT_APP_ORION_HOST || "http://localhost:6001";
 
 export default axios.create({
     baseURL: orionBaseUrl,
