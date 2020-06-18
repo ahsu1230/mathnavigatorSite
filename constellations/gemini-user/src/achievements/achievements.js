794518d4053118546bde282e487e07920abb802e
notewio <fzheng413@gmail.com>
2020-06-18 09:31:33 -0400

    prettify

--- constellations/gemini-user/src/achievements/achievements.js
+++ constellations/gemini-user/src/achievements/achievements.js
@@ -5,25 +5,23 @@ import API from "../utils/api.js";
 
 export class YearList extends React.Component {
     render() {
-		let a = this.props.achievements
-		let year = this.props.year
-		let achievements = a.map((achievement, index) => (
-			<li key={achievement.id}>
-				<div className="bullet"></div>
-				<p>{achievement.message}</p>
-			</li>
-		))
-		
+        let a = this.props.achievements;
+        let year = this.props.year;
+        let achievements = a.map((achievement, index) => (
+            <li key={achievement.id}>
+                <div className="bullet"></div>
+                <p>{achievement.message}</p>
+            </li>
+        ));
+
         return (
-			<div className="achieve-card">
-				<h2>{year}</h2>
-				
-				<ul>
-					{achievements}
-				</ul>
-				
-				<a href="#/programs">View our Programs &gt;</a>
-			</div>
+            <div className="achieve-card">
+                <h2>{year}</h2>
+
+                <ul>{achievements}</ul>
+
+                <a href="#/programs">View our Programs &gt;</a>
+            </div>
         );
     }
 }
@@ -42,40 +40,40 @@ export class AchievementPage extends React.Component {
         });
     }
 
-	// {"id":1,"publishedAt":null,"year":2020,"message":"message"}
+    // {"id":1,"publishedAt":null,"year":2020,"message":"message"}
     render() {
-		const achievements = this.state.achieveList
-		
-		let sorted = {}
-		achievements.forEach( achievement => {			
-			if (!(achievement.year in sorted)) {
-				sorted[achievement.year] = []
-			}
-			sorted[achievement.year].push(achievement)
-		} )
-		
-		let years = Object.keys(sorted)
-		years.sort().reverse()
-		
-		let items = []
-		years.forEach(year => {
-			items.push((
-				<YearList key={year} year={year} achievements={sorted[year]} />
-			))
-		})
-		
+        const achievements = this.state.achieveList;
+
+        let sorted = {};
+        achievements.forEach((achievement) => {
+            if (!(achievement.year in sorted)) {
+                sorted[achievement.year] = [];
+            }
+            sorted[achievement.year].push(achievement);
+        });
+
+        let years = Object.keys(sorted);
+        years.sort().reverse();
+
+        let items = [];
+        years.forEach((year) => {
+            items.push(
+                <YearList key={year} year={year} achievements={sorted[year]} />
+            );
+        });
+
         return (
             <div id="view-achieve">
-				<div id="view-achievements-container">
-					<h1>Math Navigator Achievements</h1>
-					
-					<div className="subheaders">
-						<h3>Congratulations to our students!</h3>
-						<h3>With their hard work, we all succeed!</h3>
-					</div>
-					
-					{items}
-				</div>
+                <div id="view-achievements-container">
+                    <h1>Math Navigator Achievements</h1>
+
+                    <div className="subheaders">
+                        <h3>Congratulations to our students!</h3>
+                        <h3>With their hard work, we all succeed!</h3>
+                    </div>
+
+                    {items}
+                </div>
             </div>
         );
     }
