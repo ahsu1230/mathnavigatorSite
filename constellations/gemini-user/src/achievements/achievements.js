4f4e42a75db455defbb28d6d1692fd9b37a6247a
notewio <fzheng413@gmail.com>
2020-06-17 20:06:08 -0400

    fix achievements page

--- constellations/gemini-user/src/achievements/achievements.js
+++ constellations/gemini-user/src/achievements/achievements.js
@@ -3,6 +3,31 @@ require("./achievements.sass");
 import React from "react";
 import API from "../utils/api.js";
 
+export class YearList extends React.Component {
+    render() {
+		let a = this.props.achievements
+		let year = this.props.year
+		let achievements = a.map((achievement, index) => (
+			<li key={achievement.id}>
+				<div className="bullet"></div>
+				<p>{achievement.message}</p>
+			</li>
+		))
+		
+        return (
+			<div className="achieve-card">
+				<h2>{year}</h2>
+				
+				<ul>
+					{achievements}
+				</ul>
+				
+				<a href="#/programs">View our Programs &gt;</a>
+			</div>
+        );
+    }
+}
+
 export class AchievementPage extends React.Component {
     state = {
         achieveList: [],
@@ -17,11 +42,40 @@ export class AchievementPage extends React.Component {
         });
     }
 
+	// {"id":1,"publishedAt":null,"year":2020,"message":"message"}
     render() {
+		const achievements = this.state.achieveList
+		
+		let sorted = {}
+		achievements.forEach( achievement => {			
+			if (!(achievement.year in sorted)) {
+				sorted[achievement.year] = []
+			}
+			sorted[achievement.year].push(achievement)
+		} )
+		
+		let years = Object.keys(sorted)
+		years.sort().reverse()
+		
+		let items = []
+		years.forEach(year => {
+			items.push((
+				<YearList key={year} year={year} achievements={sorted[year]} />
+			))
+		})
+		
         return (
             <div id="view-achieve">
-                <h1>Math Navigator Achievements</h1>
-                <p>{JSON.stringify(this.state.achieveList)}</p>
+				<div id="view-achievements-container">
+					<h1>Math Navigator Achievements</h1>
+					
+					<div className="subheaders">
+						<h3>Congratulations to our students!</h3>
+						<h3>With their hard work, we all succeed!</h3>
+					</div>
+					
+					{items}
+				</div>
             </div>
         );
     }
