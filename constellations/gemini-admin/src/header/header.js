bcbf3d34c9a1b2d731ad4118dd92ba20fb0658fb
Daniel Liu <daniel.g.liu14@gmail.com>
2020-06-20 14:02:05 -0400

    Added a help page

--- constellations/gemini-admin/src/header/header.js
+++ constellations/gemini-admin/src/header/header.js
@@ -42,7 +42,7 @@ export class HeaderSection extends React.Component {
                 <div id="header-left">
                     <HeaderDropdown id={"site"} title={"Site"} />
                     <HeaderLink title={"Accounts"} url={"/accounts"} />
-                    <HeaderLink title={"Emails"} url={"/emails"} />
+                    <HeaderLink title={"Help"} url={"/help"} />
                     <span>Sign Out</span>
                 </div>
             </div>
---
+++ constellations/gemini-admin/src/help/help.js
@@ -0,0 +1,21 @@
+"use strict";
+require("./help.styl");
+import React from "react";
+
+export class HelpPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>How to Create a Class</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h1>How to Email Users</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <h1>How to use the Dashboard</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+        );
+        }
+}
