a5c7f7a4d95865f085ad570138b4bd4f6565673e
notewio <fzheng413@gmail.com>
2020-06-25 14:28:33 -0400

    fix check&uncheck error

--- constellations/gemini-admin/src/announce/announce.js
+++ constellations/gemini-admin/src/announce/announce.js
@@ -9,7 +9,7 @@ export class AnnouncePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            list: []
+            list: [],
         };
     }
 
@@ -19,35 +19,52 @@ export class AnnouncePage extends React.Component {
             this.setState({ list: announcements });
         });
     }
-	
-	onChangeCheckbox = (e) => {
-		let successCallback = () => console.log('api success')
-        let failCallback = (err) =>
+
+    onChangeCheckbox = (e) => {
+        let successCallback = () => console.log("api success");
+        let failCallback = (err) => {
             alert("Could not save announcement: " + err.response.data);
-			
-		let previous = this.state.list.findIndex(announcement => announcement.onHomePage)
-		let current = this.state.list.findIndex(announcement => announcement.id == e.target.id)
-		
-		this.state.list[current].onHomePage = true
-		if (previous >= 0) { this.state.list[previous].onHomePage = false }
-		
-		[previous, current].forEach(index => {
-			if (index >= 0) {
-				API.post(
-					"api/announcements/announcement/" + this.state.list[index].id,
-					this.state.list[index]
-				).then((res) => successCallback())
-				.catch((err) => failCallback(err));
-			}
-		})
-		this.setState(this.state)
-	}
+            console.log(err.response);
+        };
+
+        let previous = this.state.list.findIndex(
+            (announcement) => announcement.onHomePage
+        );
+        let current = this.state.list.findIndex(
+            (announcement) => announcement.id == e.target.id
+        );
+
+        this.state.list[current].onHomePage = true;
+        if (previous >= 0) {
+            this.state.list[previous].onHomePage = false;
+        }
+
+        this.setState(this.state);
+
+        let indexes = previous == current ? [previous] : [previous, current];
+
+        indexes.forEach((index) => {
+            if (index >= 0) {
+                API.post(
+                    "api/announcements/announcement/" +
+                        this.state.list[index].id,
+                    this.state.list[index]
+                )
+                    .then((res) => successCallback())
+                    .catch((err) => failCallback(err));
+            }
+        });
+    };
 
     render() {
         const rows = this.state.list.map((row, index) => {
             return (
                 <li key={index}>
-                    <AnnounceRow key={index} row={row} selected={this.state.selected} onChangeCheckbox={this.onChangeCheckbox}/>
+                    <AnnounceRow
+                        key={index}
+                        row={row}
+                        onChangeCheckbox={this.onChangeCheckbox}
+                    />
                 </li>
             );
         });
@@ -57,7 +74,7 @@ export class AnnouncePage extends React.Component {
                 <h1>All Announcements ({numRows}) </h1>
 
                 <ul className="announce-list-row subheader">
-					<li className="li-med">On Home Page</li>
+                    <li className="li-med">On Home Page</li>
                     <li className="li-med">State</li>
                     <li className="li-med">Date</li>
                     <li className="li-med">Author</li>
@@ -79,8 +96,8 @@ class AnnounceRow extends React.Component {
     render() {
         const announceId = this.props.row.id;
         const postedAt = moment(this.props.row.postedAt);
-		
-		const checked = this.props.row.onHomePage || false;
+
+        const checked = this.props.row.onHomePage || false;
 
         const now = moment();
         const isScheduled = postedAt.isAfter(now);
@@ -91,9 +108,14 @@ class AnnounceRow extends React.Component {
         const url = "/announcements/" + announceId + "/edit";
         return (
             <ul className="announce-list-row">
-				<li className="li-med">
-					<input type="checkbox" onChange={this.props.onChangeCheckbox} id={announceId} checked={checked}/>
-				</li>
+                <li className="li-med">
+                    <input
+                        type="checkbox"
+                        onChange={this.props.onChangeCheckbox}
+                        id={announceId}
+                        checked={checked}
+                    />
+                </li>
                 <li
                     className={
                         "li-med " + (isScheduled ? " scheduled" : " published")
