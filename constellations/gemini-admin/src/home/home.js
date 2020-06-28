2107f314479b5f8a006c14a6ac7e1e73af3c9a67
Serena Xu <sx.serena.xu@gmail.com>
2020-06-28 15:40:38 -0400

    view details to class

--- constellations/gemini-admin/src/home/home.js
+++ constellations/gemini-admin/src/home/home.js
@@ -15,10 +15,8 @@ export class HomePage extends React.Component {
                         <span>
                             <h3>Unpublished Classes</h3>{" "}
                         </span>
-                        <button id="publish"> 
-                            <Link to={"/classes"}>
-                                View Details to Publish
-                            </Link>
+                        <button id="publish">
+                            <Link to={"/classes"}>View Details to Publish</Link>
                         </button>
                     </div>
 
@@ -42,31 +40,31 @@ export class HomePage extends React.Component {
                     </div>
                 </div>
 
-
                 <div class="section">
-                
                     <h3>New Users</h3>
                     <div class="container-user">
                         <p>
                             Lorem ipsum dolor sit amet, vim ei tota dicant
                             interpretaris, sea consulatu scripserit ei, pri eu
-                            accumsan contentiones. Usu ei iriure deleniti. Cum ut
-                            periculis laboramus referrentur. Vis ea laoreet
-                            imperdiet deterruisset, probo pertinax iudicabit qui ea.
-                            Duo liber quodsi contentiones ne, vel ei movet
+                            accumsan contentiones. Usu ei iriure deleniti. Cum
+                            ut periculis laboramus referrentur. Vis ea laoreet
+                            imperdiet deterruisset, probo pertinax iudicabit qui
+                            ea. Duo liber quodsi contentiones ne, vel ei movet
                             conceptam, id his quod iriure feugiat. Qui ex vide
-                            labitur volumus. His cu fugit adolescens voluptatibus,
-                            et per torquatos interesset. Iusto deleniti invenire id
-                            cum. Te saepe alterum appellantur pro, in est errem
-                            dicant suscipit, oblique argumentum sed et. Quo cu
-                            partem inermis, vix et minimum vivendum, ut illud
-                            delectus eos. Mei te alia justo, per amet nemore quodsi
-                            cu, mel eruditi copiosae contentiones cu. Sed ea quando
-                            mediocrem, soleat dolorum no ius. Eu electram iracundia
-                            mnesarchum his, eum ut hinc latine. Est simul definiebas
-                            ut, debitis invenire eu usu. Eu eros appetere mel,
-                            nullam delenit tincidunt duo at. Eos option appetere
-                            torquatos in, cu debitis singulis principes usu.
+                            labitur volumus. His cu fugit adolescens
+                            voluptatibus, et per torquatos interesset. Iusto
+                            deleniti invenire id cum. Te saepe alterum
+                            appellantur pro, in est errem dicant suscipit,
+                            oblique argumentum sed et. Quo cu partem inermis,
+                            vix et minimum vivendum, ut illud delectus eos. Mei
+                            te alia justo, per amet nemore quodsi cu, mel
+                            eruditi copiosae contentiones cu. Sed ea quando
+                            mediocrem, soleat dolorum no ius. Eu electram
+                            iracundia mnesarchum his, eum ut hinc latine. Est
+                            simul definiebas ut, debitis invenire eu usu. Eu
+                            eros appetere mel, nullam delenit tincidunt duo at.
+                            Eos option appetere torquatos in, cu debitis
+                            singulis principes usu.
                         </p>
                     </div>
                 </div>
@@ -77,28 +75,28 @@ export class HomePage extends React.Component {
                         <p>
                             Lorem ipsum dolor sit amet, vim ei tota dicant
                             interpretaris, sea consulatu scripserit ei, pri eu
-                            accumsan contentiones. Usu ei iriure deleniti. Cum ut
-                            periculis laboramus referrentur. Vis ea laoreet
-                            imperdiet deterruisset, probo pertinax iudicabit qui ea.
-                            Duo liber quodsi contentiones ne, vel ei movet
+                            accumsan contentiones. Usu ei iriure deleniti. Cum
+                            ut periculis laboramus referrentur. Vis ea laoreet
+                            imperdiet deterruisset, probo pertinax iudicabit qui
+                            ea. Duo liber quodsi contentiones ne, vel ei movet
                             conceptam, id his quod iriure feugiat. Qui ex vide
-                            labitur volumus. His cu fugit adolescens voluptatibus,
-                            et per torquatos interesset. Iusto deleniti invenire id
-                            cum. Te saepe alterum appellantur pro, in est errem
-                            dicant suscipit, oblique argumentum sed et. Quo cu
-                            partem inermis, vix et minimum vivendum, ut illud
-                            delectus eos. Mei te alia justo, per amet nemore quodsi
-                            cu, mel eruditi copiosae contentiones cu. Sed ea quando
-                            mediocrem, soleat dolorum no ius. Eu electram iracundia
-                            mnesarchum his, eum ut hinc latine. Est simul definiebas
-                            ut, debitis invenire eu usu. Eu eros appetere mel,
-                            nullam delenit tincidunt duo at. Eos option appetere
-                            torquatos in, cu debitis singulis principes usu.
+                            labitur volumus. His cu fugit adolescens
+                            voluptatibus, et per torquatos interesset. Iusto
+                            deleniti invenire id cum. Te saepe alterum
+                            appellantur pro, in est errem dicant suscipit,
+                            oblique argumentum sed et. Quo cu partem inermis,
+                            vix et minimum vivendum, ut illud delectus eos. Mei
+                            te alia justo, per amet nemore quodsi cu, mel
+                            eruditi copiosae contentiones cu. Sed ea quando
+                            mediocrem, soleat dolorum no ius. Eu electram
+                            iracundia mnesarchum his, eum ut hinc latine. Est
+                            simul definiebas ut, debitis invenire eu usu. Eu
+                            eros appetere mel, nullam delenit tincidunt duo at.
+                            Eos option appetere torquatos in, cu debitis
+                            singulis principes usu.
                         </p>
                     </div>
                 </div>
-
-
             </div>
         );
     }
