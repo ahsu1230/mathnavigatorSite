314f9964a131c14038b08191b504d99a53c39615
Austin Hsu <austin021499@gmail.com>
2020-07-01 14:41:11 -0400

    comments deleted for get all

--- constellations/orion/src/controllers/user_controller.go
+++ constellations/orion/src/controllers/user_controller.go
@@ -8,21 +8,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// func GetAllUsers(c *gin.Context) {
-// 	// Incoming optional parameter
-// 	search := c.Query("search")
-// 	pageSize := ParseParamInt(c.Query("pageSize"), 100)
-// 	offset := ParseParamInt(c.Query("offset"), 0)
-
-// 	userList, err := repos.UserRepo.SelectAll(search, pageSize, offset)
-// 	if err != nil {
-// 		c.Error(err)
-// 		c.String(http.StatusInternalServerError, err.Error())
-// 	} else {
-// 		c.JSON(http.StatusOK, userList)
-// 	}
-// }
-
 // type UserSearchBody struct {
 // 	FirstName string
 // 	MiddleName string
