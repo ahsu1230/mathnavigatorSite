605cb5eaa384271bd531d3364d9cdea6a2dbaac7
notewio <fzheng413@gmail.com>
2020-07-02 10:27:52 -0400

    popup on home page

--- constellations/gemini-user/src/home/homeAnnounce.js
+++ constellations/gemini-user/src/home/homeAnnounce.js
@@ -4,57 +4,56 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 import { isEmpty, filter } from "lodash";
-import { getAnnouncements } from "../repos/apiRepo.js";
-import { ANNOUNCE_LAST_DISMISS } from "../storage.js";
+//import { getAnnouncements } from "../repos/apiRepo.js";
+import { ANNOUNCE_LAST_DISMISS } from "../utils/storage.js";
 const classnames = require("classnames");
-const srcClose = require("../../assets/close_black.svg");
+import srcClose from "../../assets/close_black.svg"
+import API from "../utils/api.js";
 
 export class HomeAnnounce extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             show: false,
-            targetAnnounce: {},
-            lastDismissed: 0,
+            targetAnnounce: {}
         };
-        this.handleDismiss = this.handleDismiss.bind(this);
     }
 
     componentDidMount() {
-        getAnnouncements().then((announcements) => {
-            var valid = filter(announcements, (a) => a.onHomePage);
-            var targetAnnounce = valid.length > 0 ? valid[0] : undefined;
+        console.log("api attempt ");
+        API.get("api/announcements/all").then((res) => {
+            const announceList = res.data;
+            console.log("api success!");            
+            
+            let valid = filter(announceList, a => a.onHomePage)
+            console.log(announceList)
+            let targetAnnounce = valid.length > 0 ? valid[0] : undefined;
+            
             targetAnnounce.shortMessage = shortenMessage(
                 targetAnnounce.message
             );
-
-            var localLastDismiss = localStorage.getItem(ANNOUNCE_LAST_DISMISS);
-            var lastDismissed = parseInt(localLastDismiss, 10) || 0;
-
-            this.unbindTimeout = setTimeout(
-                function () {
-                    this.setState({ show: true });
-                }.bind(this),
-                2500
-            );
+            
+            
+            let lastDismissed = localStorage.getItem(ANNOUNCE_LAST_DISMISS)
+            
+            if (lastDismissed != targetAnnounce.id) {
+                this.showTimeout = setTimeout(
+                    () => { this.setState({ show:true }) },
+                    1000
+                )
+            }
+            
 
             this.setState({
                 targetAnnounce: targetAnnounce,
-                lastDismissed: lastDismissed,
             });
         });
     }
-
-    componentWillUnmount() {
-        clearTimeout(this.unbindTimeout);
-    }
-
-    handleDismiss() {
-        this.setState({ show: false });
-        localStorage.setItem(
-            ANNOUNCE_LAST_DISMISS,
-            this.state.targetAnnounce.timestamp
-        );
+    
+    dismiss = () => {
+        this.setState({ show: false })
+        
+        localStorage.setItem(ANNOUNCE_LAST_DISMISS, this.state.targetAnnounce.id)
     }
 
     render() {
@@ -67,7 +66,7 @@ export class HomeAnnounce extends React.Component {
                     announce={announce}
                     show={this.state.show}
                     announceHeight={this.state.announceHeight}
-                    handleDismiss={this.handleDismiss}
+                    dismiss={this.dismiss}
                 />
             );
         } else {
@@ -81,18 +80,17 @@ class Popup extends React.Component {
     render() {
         const announce = this.props.announce;
         const show = this.props.show;
-        const handleDismiss = this.props.handleDismiss;
-        const announceClass = classnames({ show: show });
+        const announceClass = show ? 'show' : '';
         return (
             <div key="real" id="home-announce" className={announceClass}>
                 <h3>New Announcement!</h3>
-                <button className="close-x" onClick={handleDismiss}>
+                <button className="close-x" onClick={this.props.dismiss}>
                     <img src={srcClose} />
                 </button>
                 <div className="text-container">
                     <p>{announce.shortMessage}</p>
                 </div>
-                <Link to="/announcements" onClick={handleDismiss}>
+                <Link to="/announcements" onClick={this.props.dismiss}>
                     Read more &#62;
                 </Link>
             </div>
