b81f0f6dfd1d6281d7ee915c4792a9b99684b3f2
Chujia Guo Local <herobrinevinyl@gmail.com>
2020-07-13 09:45:53 -0400

    Domain files

--- constellations/orion/src/domains/transaction.go
+++ constellations/orion/src/domains/transaction.go
@@ -2,30 +2,32 @@ package domains
 
 import (
 	"errors"
-	"regexp"
 	"time"
 )
 
 var TABLE_TRANSACTIONS = "transactions"
 
 type Transaction struct {
-	Id              int       `json:"id"`
-	CreatedAt       time.Time  `json:"-" db:"created_at"`
-	Amount			uint	   `json:"amount"`
-	PaymentType	    string	   `json:"paymentType" db:"payment_type"`
+	Id          int       `json:"id"`
+	CreatedAt   time.Time `json:"-" db:"created_at"`
+	Amount      int       `json:"amount"`
+	PaymentType string    `json:"paymentType" db:"payment_type"`
 }
 
 func (transaction *Transaction) Validate() error {
-	id := transaction.Id
 	amount := transaction.Amount
 	paymentType := transaction.PaymentType
 
-	if paymentType != "pay_paypal" || paymentType != "pay_check" || paymentType != "pay_cash" || paymentType != "charging" || paymentType != "refund" {
+	if paymentType != "pay_paypal" && paymentType != "pay_check" && paymentType != "pay_cash" && paymentType != "charge" && paymentType != "refund" {
 		return errors.New("invalid payment type")
 	}
 
-	if paymentType != "refund" && amount < 0 {
-		return errrors.New("amount less than 0")
+	if paymentType != "charge" && amount < 0 {
+		return errors.New("amount less than 0")
+	}
+
+	if paymentType == "charge" && amount > 0 {
+		return errors.New("charge greater than 0")
 	}
 
 	return nil
