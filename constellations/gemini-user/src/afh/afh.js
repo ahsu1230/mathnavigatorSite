258b2861262bfe93666c040af791b2b47025c5da
Serena Xu <sx.serena.xu@gmail.com>
2020-07-17 16:36:07 -0400

    layout

---
+++ constellations/gemini-user/src/afh/afh.js
@@ -0,0 +1,71 @@
+"use strict";
+require("./afh.sass");
+import React from "react";
+import API from "../utils/api.js";
+
+export class AFHPage extends React.Component {
+    state = {
+        currentTab: "",
+    };
+
+    openSubject = (subjectName) => {
+        this.setState({
+            currentTab: subjectName,
+        });
+    };
+
+    render() {
+        return (
+            <div id="view-afh">
+                <h1>Ask for Help</h1>
+                <div className="description">
+                    We provide free sessions for students to ask for additional
+                    assistance on any of our program subjects. Please fill the
+                    form to let us know you are coming. You must be registered
+                    with one of our programs to attend.
+                </div>
+
+                <h1>Ask for Help Sessions by Subject</h1>
+                <div class="tab">
+                    <button
+                        className="tabButton"
+                        onClick= {() => this.openSubject("Math")}
+                        id="defaultOpen">
+                        Math
+                    </button>
+                    <button
+                        className="tabButton"
+                        onClick= {() => this.openSubject("Eng")}>
+                        English
+                    </button>
+                    <button
+                        className="tabButton"
+                        onClick= {() => this.openSubject("Comp")}>
+                        Computer Programming
+                    </button>
+                </div>
+
+                <div
+                    className={
+                        this.state.currentTab == "Math" ? "showTab" : "hide"
+                    }>
+                    <p>testing 1</p>
+                </div>
+
+                <div
+                    className={
+                        this.state.currentTab == "Eng" ? "showTab" : "hide"
+                    }>
+                    <p>testing 2</p>
+                </div>
+                
+                <div
+                    className={
+                        this.state.currentTab == "Comp" ? "showTab" : "hide"
+                    }>
+                    <p>testing 3</p>
+                </div>
+            </div>
+        );
+    }
+}
