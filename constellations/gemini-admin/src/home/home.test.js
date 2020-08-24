3bde1640a9beb5d1adff448bbe4fb26b65f2a86e
Serena Xu <sx.serena.xu@gmail.com>
2020-08-24 16:43:19 -0400

    changes

--- constellations/gemini-admin/src/home/home.test.js
+++ constellations/gemini-admin/src/home/home.test.js
@@ -5,6 +5,11 @@ import { HomePage } from "./home.js";
 describe("Home Page", () => {
     const component = shallow(<HomePage />);
 
+    const TAB_CLASSES = "classes";
+    const TAB_USERS = "users";
+    const TAB_REGISTRATIONS = "registrations";
+    const TAB_UNPAID = "unpaid";
+
     test("renders", () => {
         expect(component.exists()).toBe(true);
         expect(component.find("h1").text()).toContain(
@@ -12,14 +17,16 @@ describe("Home Page", () => {
         );
 
         expect(component.find("TabButton").at(0).prop("section")).toBe(
-            "clasess"
+            TAB_CLASSES
         );
-        expect(component.find("TabButton").at(1).prop("section")).toBe("users");
+        expect(component.find("TabButton").at(1).prop("section")).toBe(
+            TAB_USERS
+        );
         expect(component.find("TabButton").at(2).prop("section")).toBe(
-            "registrations"
+            TAB_REGISTRATIONS
         );
         expect(component.find("TabButton").at(3).prop("section")).toBe(
-            "unpaid"
+            TAB_UNPAID
         );
         expect(component.find("TabButton").length).toBe(4);
     });
--- constellations/gemini-user/src/internship/internship.test.js
+++ constellations/gemini-user/src/internship/internship.test.js
@@ -7,13 +7,16 @@ describe("Internship Page", () => {
 
     test("renders", () => {
         expect(component.exists()).toBe(true);
-        expect(component.find("h1").text()).toContain(
+
+        expect(component.find("h1").at(0).text()).toContain(
             "Software Development Internship"
         );
-        expect(component.find("h1").text()).toContain("Technology Stack");
-        expect(component.find("h1").text()).toContain("Internship Structure");
+        expect(component.find("h1").at(1).text()).toContain("Technology Stack");
+        expect(component.find("h1").at(2).text()).toContain(
+            "Internship Structure"
+        );
         expect(component.find("h4").text()).toContain("Past Interns:");
-        expect(component.find("h1").text()).toContain(
+        expect(component.find("h1").at(3).text()).toContain(
             "Math Navigator Products"
         );
     });
