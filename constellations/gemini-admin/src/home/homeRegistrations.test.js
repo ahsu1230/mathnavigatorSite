b9f6e8a0f5a0928789055e7c05ef770a54dd02ec
Serena Xu <sx.serena.xu@gmail.com>
2020-08-26 10:27:57 -0400

    registration

--- constellations/gemini-admin/src/home/homeRegistrations.test.js
+++ constellations/gemini-admin/src/home/homeRegistrations.test.js
@@ -7,13 +7,13 @@ describe("Home Registrations", () => {
 
     test("renders", () => {
         expect(component.exists()).toBe(true);
-        expect(component.find("h3").text()).toContain(
+        expect(component.find("h3").at(0).text()).toContain(
             "Pending Registrations For Classes"
         );
-        expect(component.find("Link").text()).toContain("View By Class");
-        expect(component.find("h3").text()).toContain(
+        expect(component.find("Link").at(0).text()).toContain("View By Class");
+        expect(component.find("h3").at(1).text()).toContain(
             "New Registrations For AFH"
         );
-        expect(component.find("Link").text()).toContain("View By AFH Session");
+        expect(component.find("Link").at(1).text()).toContain("View By AFH Session");
     });
 });
