9e6667e41f435347329e729a1b227f0141285d71
Aaron Hsu <aaron@nextbit.com>
2020-10-30 23:35:38 -0400

    redo intern page

---
+++ constellations/gemini-user/src/internship/pastInterns.js
@@ -0,0 +1,44 @@
+"use strict";
+
+export const PastInterns = [
+    {
+        name: "Cathy Y.",
+        school: "Richard Montgomery HS 2020",
+        college: "Massachusetts Institute of Technology",
+    },
+    {
+        name: "Serena X.",
+        school: "Winston Churchill HS 2020",
+        college: "University of Pennsylvania",
+    },
+    {
+        name: "Jessica Y.",
+        school: "Montgomery Blair HS 2020",
+        college: "University of Maryland",
+    },
+    {
+        name: "Austin H.",
+        school: "Winston Churchill HS 2017",
+        college: "Carnegie Mellon University",
+    },
+    {
+        name: "Tony W.",
+        school: "Richard Montgomery HS 2021",
+    },
+    {
+        name: "Daniel L.",
+        school: "Winston Churchill HS 2021",
+    },
+    {
+        name: "Max Z.",
+        school: "Montgomery Blair HS 2021",
+    },
+    {
+        name: "Chujia G.",
+        school: "Montgomery Blair HS 2022",
+    },
+    {
+        name: "Frederick Z.",
+        school: "Montgomery Blair HS 2023",
+    },
+];
